9dc8b9c6aa1e6beefc195ed7957fce393d21e3ef
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-06-04 15:53:49 +0300

    Fix email confirmation sender

--- src/components/pages/RegisterPage/RegisterPage.tsx
+++ src/components/pages/RegisterPage/RegisterPage.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './RegisterPage.css';
 import avatar from '../../../assets/avatar.png';
+import Logo from '../../common/Logo.tsx';
 
 const { Link } = Typography;
 
@@ -41,6 +42,12 @@ const RegisterPage = () => {
    const onFinish = (values: IRegisterModel) => {
       const formData = new FormData();
 
+      Object.keys(values).forEach(key => {
+         if (values[key] !== undefined) {
+            formData.append(key, values[key]);
+         }
+      });
+
       axios.post('http://localhost:5181/api/Authentication/register', formData, {
          headers: {
             'Content-Type': 'multipart/form-data'
@@ -50,6 +57,7 @@ const RegisterPage = () => {
       }).catch(error => {
          console.log(error);
       });
+      window.location.href = '/email-confirmation-required';
    };
 
    const onFinishFailed = (errorInfo: any) => {
@@ -70,6 +78,9 @@ const RegisterPage = () => {
 
    return (
       <Row justify="center" align="middle" className="register-page">
+         <div className="logo-container">
+            <Logo />
+         </div>
          {/*<Col xs={24} sm={20} md={16} lg={12} xl={8}>*/}
          <Col xs={24} sm={20} md={16} lg={14} xl={12}>
 
@@ -296,10 +307,7 @@ const RegisterPage = () => {
 
 
                      <Form.Item>
-                        {/*<Button htmlType="submit" style={{ width: '100%' }}>
-                           Register
-                        </Button>*/}
-                        <Button href='/email-confirmation-required' style={{ width: '100%' }}>
+                        <Button htmlType="submit" style={{ width: '100%' }}>
                            Register
                         </Button>
                      </Form.Item>
