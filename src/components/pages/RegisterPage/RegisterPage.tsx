71c48eed71d134b9a42790082eb64702798bb24f
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-06-07 13:11:27 +0300

    fix error with logo

--- src/components/pages/RegisterPage/RegisterPage.tsx
+++ src/components/pages/RegisterPage/RegisterPage.tsx
@@ -7,7 +7,6 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './RegisterPage.css';
 import avatar from '../../../assets/avatar.png';
-import Logo from '../../common/Logo.tsx';
 
 const { Link } = Typography;
 
@@ -78,9 +77,6 @@ const RegisterPage = () => {
 
    return (
       <Row justify="center" align="middle" className="register-page">
-         <div className="logo-container">
-            <Logo />
-         </div>
          {/*<Col xs={24} sm={20} md={16} lg={12} xl={8}>*/}
          <Col xs={24} sm={20} md={16} lg={14} xl={12}>
 
