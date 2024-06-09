1ec7f394afe5bf636ae0e809dd671b18f3215c2a
kutukov2000 <kutukov16.2005@gmail.com>
2024-06-09 10:55:43 +0300

    feat: autologin ✨

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import './index.css'
+import { getLocalStorage } from './utils/storage/localStorageUtils.ts';
+import { isTokenActive } from './utils/storage/isTokenActive.ts';
+import { autoLogin } from './store/account/account.slice.ts';
 
+const token = getLocalStorage('authToken');
+if (typeof token === 'string') {
+   if (isTokenActive(token)) {
+      store.dispatch(autoLogin(token));
+   }
+}
 ReactDOM.createRoot(document.getElementById('root')!).render(
    <Router>
       <Provider store={store}>
