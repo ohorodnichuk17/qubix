db4c025c756a9f4dc5cc333d1c50f5cf22b53c98
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-06-13 00:19:00 +0300

    Navbar and footer upgrade

--- src/components/containers/HeaderDefault.tsx
+++ src/components/containers/HeaderDefault.tsx
@@ -1,13 +1,24 @@
-import { Button, Input, Layout } from 'antd';
-import { Link } from 'react-router-dom';
-import { UserOutlined, UserAddOutlined, SearchOutlined } from '@ant-design/icons';
-import ButtonGroup from "antd/es/button/button-group";
+import React, { useState } from 'react';
+import { Button, Input, Layout, Avatar, Tooltip, Dropdown, Menu } from 'antd';
+import { Link, NavLink } from 'react-router-dom';
+import { SearchOutlined, UserOutlined, UserAddOutlined } from '@ant-design/icons';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { logout } from '../../store/account/account.slice';
-import './HeaderDefault.css'
+import './HeaderDefault.css';
 import logo from '../../assets/authentication/logotype.png';
+import home from '../../assets/profile/home.png';
+import activeHome from '../../assets/profile/active_home.png';
+import friends from '../../assets/profile/friends.png';
+import activeFriends from '../../assets/profile/active_friends.png';
+import story from '../../assets/profile/story.png';
+import activeStory from '../../assets/profile/active_story.png';
+import messanger from '../../assets/profile/messanger.png';
+import activeMessanger from '../../assets/profile/messanger_active.png';
+import glyph from '../../assets/profile/glyph.png';
 
 const { Header } = Layout;
+
+
 
 const HeaderDefault = () => {
    const dispatch = useAppDispatch();
@@ -17,65 +28,99 @@ const HeaderDefault = () => {
       dispatch(logout());
    };
 
+   const menu = (
+      <Menu>
+         <Menu.Item key="1">
+            <NavLink to="/settings">
+               Settings and privacy
+            </NavLink>
+         </Menu.Item>
+         <Menu.Item key="2" onClick={handleLogout}>
+            Logout
+         </Menu.Item>
+      </Menu>
+   );
+
    return (
       <Header className="custom-header">
-         <div className="left-section">
-            <Input
-               className="search-box"
-               placeholder="Search"
-               prefix={<SearchOutlined style={{ color: '#000000' }} />}
-            />
-            <div className="header-gradient">
-               <img src={logo} alt="logo" className="header-logo" />
-               <div className="header-text">
-                  <span className='header-text-regular'>This is</span>
-                  <span className="header-text-bold">Quilt</span>
-               </div>
-            </div>
-         </div>
-
          {isLogin ? (
-            <ButtonGroup size="large" style={{ gap: '20px' }}>
-               <Button className="minimalist-button">
-                  {`${user?.firstName} ${user?.lastName}`}
-               </Button>
-               <Button>
-                  <Link to="/story">
-                     Create story
-                  </Link>
-               </Button>
-               <Button type='link' onClick={() => handleLogout()} className="minimalist-button">
-                  Logout
-               </Button>
-               <Button type='link' className="minimalist-button">
-                  <Link to='/change-email'>
-                     Change Email
-                  </Link>
-               </Button>
-               <Button type='link' className="minimalist-button">
-                  <Link to='/forgot-password'>
-                     Forgot password
-                  </Link>
-               </Button>
-               <Button type='link' className="minimalist-button">
-                  <Link to='/reset-password'>
-                     Reset password
-                  </Link>
-               </Button>
-            </ButtonGroup>
+            <>
+               <div className="navbar-left">
+                  <img src={logo} alt="logo" className="logo" />
+                  <Input
+                     className="search-bar"
+                     placeholder="Search"
+                     prefix={<SearchOutlined style={{ color: '#000000' }} />}
+                  />
+               </div>
+               <div className="navbar-center">
+                  <Tooltip title="Home">
+                     <NavLink to="/" className="nav-icon home-icon-link" activeClassName="active">
+                        {({ isActive }) => (
+                           <img src={isActive ? activeHome : home} alt="Home" className="nav-icon-img home-icon" />
+                        )}
+                     </NavLink>
+                  </Tooltip>
+                  <Tooltip title="Friends">
+                     <NavLink to="/friends" className="nav-icon friends-icon-link" activeClassName="active">
+                        {({ isActive }) => (
+                           <img src={isActive ? activeFriends : friends} alt="Friends" className="nav-icon-img friends-icon" />
+                        )}
+                     </NavLink>
+                  </Tooltip>
+                  <Tooltip title="Create New Story">
+                     <NavLink to="/story" className="nav-icon group-icon-link" activeClassName="active">
+                        {({ isActive }) => (
+                           <img src={isActive ? activeStory : story} alt="Story" className="nav-icon-img group-icon" />
+                        )}
+                     </NavLink>
+                  </Tooltip>
+               </div>
+               <div className="navbar-right">
+                  <Tooltip title="Messenger">
+                     <NavLink to="/messenger" className="nav-icon messanger-icon-link" activeClassName="active">
+                        {({ isActive }) => (
+                           <img src={isActive ? activeMessanger : messanger} alt="Messenger" className="nav-icon-img messanger-icon" />
+                        )}
+                     </NavLink>
+                  </Tooltip>
+                  <div className="avatar-dropdown-container">
+                     <Avatar src={`http://localhost:5181${user?.avatar}`} size={50} />
+                     <Dropdown overlay={menu} trigger={['click']}>
+                        <Button className="avatar-dropdown-button" icon={<img src={glyph} alt="dropdown" />} />
+                     </Dropdown>
+                  </div>
+               </div>
+            </>
          ) : (
-            <div style={{ display: 'flex', gap: '20px' }}>
-               <Link to="/login" style={{ color: '#FFFAFA', textDecoration: 'none' }}>
-                  <Button icon={<UserOutlined />} className="minimalist-button login">
-                     Login
-                  </Button>
-               </Link>
-               <Link to="/register" style={{ color: '#FFFAFA', textDecoration: 'none' }}>
-                  <Button icon={<UserAddOutlined />} className="minimalist-button register">
-                     Register
-                  </Button>
-               </Link>
-            </div>
+            <>
+               <div className="left-section">
+                  <Input
+                     className="search-bar"
+                     placeholder="Search"
+                     prefix={<SearchOutlined style={{ color: '#000000' }} />}
+                  />
+               </div>
+               <div className="center-section">
+                  <img src={logo} alt="logo" className="header-logo" />
+                  <div className="header-text">
+                     <span className='header-text-regular'>This is</span>
+                     <span className="header-text-bold">Quilt</span>
+                  </div>
+               </div>
+               <div className="right-section">
+                  <NavLink to="/login" style={{ color: '#FFFAFA', textDecoration: 'none' }}>
+                     <Button icon={<UserOutlined />} className="minimalist-button login">
+                        Login
+                     </Button>
+                  </NavLink>
+                  <NavLink to="/register" style={{ color: '#FFFAFA', textDecoration: 'none' }}>
+                     <Button icon={<UserAddOutlined />} className="minimalist-button register">
+                        Register
+                     </Button>
+                  </NavLink>
+               </div>
+            </>
          )}
       </Header>
    );
