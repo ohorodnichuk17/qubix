21466ab3f31b95dca5bf2395ae5038b18d7c7ffb
kutukov2000 <kutukov16.2005@gmail.com>
2024-06-27 11:48:30 +0300

    refactor: useCapture using biome js

--- src/components/pages/Story/CreateStoryPage/hooks/useCapture.tsx
+++ src/components/pages/Story/CreateStoryPage/hooks/useCapture.tsx
@@ -1,40 +1,51 @@
 import { message } from "antd";
 import { useRef } from "react";
-import domtoimage from 'dom-to-image';
-import { StoryType } from "../types";
+import domtoimage from "dom-to-image";
+import type { StoryType } from "../types";
 
 const useCapture = () => {
-    const captureAreaRef = useRef(null);
+	const captureAreaRef = useRef(null);
 
-    const getCapture = async (storyType: StoryType,isStory=true) => {
-        if (captureAreaRef.current == null) {
-            message.error("Create story error!");
-            return null;
-        }
+	const getCapture = async (storyType: StoryType, isStory = true) => {
+		if (captureAreaRef.current == null) {
+			message.error("Create story error!");
+			return null;
+		}
 
-        if (storyType == "text" && isStory)
-            (captureAreaRef.current as HTMLDivElement).innerHTML = '<div class="preview-div-bordered"></div>';
+		if (storyType === "text" && isStory)
+			(captureAreaRef.current as HTMLDivElement).innerHTML =
+				'<div class="preview-div-bordered"></div>';
 
-        try {
-            const dataUrl = await domtoimage.toJpeg(captureAreaRef.current, { quality: 1, style: { 'overflow': 'hidden' } });
-            const blob = dataURLToBlob(dataUrl);
-            return blob;
-        } catch (error) {
-            message.error("Error capturing image");
-            return null;
-        }
-    };
+		try {
+			const dataUrl = await domtoimage.toJpeg(captureAreaRef.current, {
+				quality: 1,
+				style: { overflow: "hidden" },
+			});
+			const blob = dataURLToBlob(dataUrl);
+			return blob;
+		} catch (error) {
+			message.error("Error capturing image");
+			return null;
+		}
+	};
 
-    const dataURLToBlob = (dataUrl: any) => {
-        let arr = dataUrl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-        while (n--) {
-            u8arr[n] = bstr.charCodeAt(n);
-        }
-        return new Blob([u8arr], { type: mime });
-    };
+	const dataURLToBlob = (dataUrl: string) => {
+		const arr = dataUrl.split(",");
+		const mimeMatch = arr[0].match(/:(.*?);/);
+		if (!mimeMatch) {
+			throw new Error("Invalid data URL");
+		}
+		const mime = mimeMatch[1];
+		const bstr = atob(arr[1]);
+		let n = bstr.length;
+		const u8arr = new Uint8Array(n);
+		while (n--) {
+			u8arr[n] = bstr.charCodeAt(n);
+		}
+		return new Blob([u8arr], { type: mime });
+	};
 
-    return { captureAreaRef, getCapture };
-}
+	return { captureAreaRef, getCapture };
+};
 
 export default useCapture;
