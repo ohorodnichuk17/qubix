653c2969b1a2c3e0b8e699f060898348b500eb87
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-03 19:01:20 +0300

    refactor: move isLogin check outside return statement 📦

--- src/components/pages/UserProfilePage/UserProfilePage.tsx
+++ src/components/pages/UserProfilePage/UserProfilePage.tsx
@@ -187,155 +187,155 @@ const UserProfilePage: React.FC = () => {
     console.log('Failed:', errorInfo);
   };
 
+  if (!isLogin) {
+    return (
+      <></>
+    );
+  }
+
   return (
-    <>
-      {isLogin ? (
-        <div style={{ backgroundColor: '#EDE0F5', padding: 0, minHeight: '100vh' }}>
-          <Row justify="center" align="middle">
-            <Card style={{ width: '100%', maxWidth: '1200px', background: 'transparent', border: "none", boxShadow: 'none' }}>
-              <Form
-                name="userProfile"
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                requiredMark={false}
+    <div style={{ backgroundColor: '#EDE0F5', padding: 0, minHeight: '100vh' }}>
+      <Row justify="center" align="middle">
+        <Card style={{ width: '100%', maxWidth: '1200px', background: 'transparent', border: "none", boxShadow: 'none' }}>
+          <Form
+            name="userProfile"
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            requiredMark={false}
+          >
+            <Form.Item
+              name="coverPhoto"
+              valuePropName="file"
+              getValueFromEvent={(e: UploadChangeParam) => {
+                const image = e?.fileList[0] as IUploadedFile;
+                return image?.originFileObj;
+              }}
+            >
+              <div
+                className='box'
+                style={{
+                  backgroundColor: '#D9D9D9',
+                  height: '40vh',
+                  borderRadius: '10px',
+                  backgroundImage: `url(${coverPhoto})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  position: 'relative'
+                }}
               >
-                <Form.Item
-                  name="coverPhoto"
-                  valuePropName="file"
-                  getValueFromEvent={(e: UploadChangeParam) => {
-                    const image = e?.fileList[0] as IUploadedFile;
-                    return image?.originFileObj;
-                  }}
-                >
-                  <div
-                    className='box'
-                    style={{
-                      backgroundColor: '#D9D9D9',
-                      height: '40vh',
-                      borderRadius: '10px',
-                      backgroundImage: `url(${coverPhoto})`,
-                      backgroundSize: 'cover',
-                      backgroundPosition: 'center',
-                      position: 'relative'
+                {coverPhoto === editImg ? (
+                  <Upload
+                    showUploadList={false}
+                    beforeUpload={() => false}
+                    accept="image/*"
+                    onChange={handleCoverPhotoChange}
+                    maxCount={1}
+                    defaultFileList={[]}
+                  >
+                    <CoverButton style={{ display: "flex", alignItems: "center", border: "none", right: "0px", bottom: "0px", position: "absolute" }}>
+                      <img src={cameraImg} alt="coverPhoto" style={{ width: 26, height: 22, margin: 5 }} />
+                      Add cover photo
+                    </CoverButton>
+                  </Upload>
+                ) : (
+                  <Dropdown overlay={coverPhotoMenu} trigger={['click']}>
+                    <CoverButton style={{ display: "flex", alignItems: "center", border: "none", right: "0px", bottom: "0px", position: "absolute" }}>
+                      <img src={editImg} alt="editCoverPhoto" style={{ width: 26, height: 22, margin: 5 }} />
+                      Edit cover photo
+                    </CoverButton>
+                  </Dropdown>
+                )}
+              </div>
+            </Form.Item>
+            <Row style={{ marginTop: '-7%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Col xs={24} sm={24} md={8} lg={8} xl={8} style={{ marginBottom: '1rem' }}>
+                <div>
+                  <Form.Item
+                    name="avatar"
+                    valuePropName="file"
+                    getValueFromEvent={(e: UploadChangeParam) => {
+                      const image = e?.fileList[0] as IUploadedFile;
+                      return image?.originFileObj;
                     }}
                   >
-                    {coverPhoto === editImg ? (
-                      <Upload
-                        showUploadList={false}
-                        beforeUpload={() => false}
-                        accept="image/*"
-                        onChange={handleCoverPhotoChange}
-                        maxCount={1}
-                        defaultFileList={[]}
-                      >
-                        <CoverButton style={{ display: "flex", alignItems: "center", border: "none", right: "0px", bottom: "0px", position: "absolute" }}>
-                          <img src={cameraImg} alt="coverPhoto" style={{ width: 26, height: 22, margin: 5 }} />
-                          Add cover photo
-                        </CoverButton>
-                      </Upload>
-                    ) : (
-                      <Dropdown overlay={coverPhotoMenu} trigger={['click']}>
-                        <CoverButton style={{ display: "flex", alignItems: "center", border: "none", right: "0px", bottom: "0px", position: "absolute" }}>
-                            <img src={editImg} alt="editCoverPhoto" style={{ width: 26, height: 22, margin: 5 }} />
-                          Edit cover photo
-                        </CoverButton>
-                      </Dropdown>
-                    )}
-                  </div>
-                </Form.Item>
-                <Row style={{ marginTop: '-7%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-                  <Col xs={24} sm={24} md={8} lg={8} xl={8} style={{ marginBottom: '1rem' }}>
-                    <div>
-                      <Form.Item
-                        name="avatar"
-                        valuePropName="file"
-                        getValueFromEvent={(e: UploadChangeParam) => {
-                          const image = e?.fileList[0] as IUploadedFile;
-                          return image?.originFileObj;
-                        }}
-                      >
-                        <Avatar size={160} src={avatar} />
-                        <Dropdown overlay={avatarMenu} trigger={['click']}>
-                          <AvatarButton style={{ border: "none", color: "black", borderRadius: "100px" }} icon={<CameraOutlined />} />
-                        </Dropdown>
+                    <Avatar size={160} src={avatar} />
+                    <Dropdown overlay={avatarMenu} trigger={['click']}>
+                      <AvatarButton style={{ border: "none", color: "black", borderRadius: "100px" }} icon={<CameraOutlined />} />
+                    </Dropdown>
+                  </Form.Item>
+                </div>
+                <Typography style={{ margin: 10, fontSize: 26 }}>{user?.email}</Typography>
+              </Col>
+              <Col xs={24} sm={24} md={16} lg={16} xl={16}>
+                <EditButton type="primary" onClick={showModal}>
+                  Edit Profile
+                </EditButton>
+                <Modal title="Edit Information" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}
+                  footer={[
+                    <Button key="cancel" onClick={handleCancel} style={{ backgroundColor: '#ff7f50', color: 'white' }}>
+                      Cancel
+                    </Button>,
+                    <Button key="submit" type="primary" form="editProfileForm" htmlType="submit">
+                      OK
+                    </Button>
+                  ]}>
+                  <Form layout="vertical" id="editProfileForm" onFinish={onFinish} onFinishFailed={onFinishFailed}>
+                    <Form.Item name="biography" label={formConfig.aboutMe.label}>
+                      <TextArea rows={4} placeholder={formConfig.aboutMe.placeholder} />
+                    </Form.Item>
+                    <Form.Item name="country" label="Country">
+                      <CountryDropdown
+                        value={country}
+                        onChange={(val) => setCountry(val)}
+                        classes="ant-select custom-select"
+                      />
+                    </Form.Item>
+                    <Form.Item name="region" label="Region">
+                      <RegionDropdown
+                        country={country}
+                        value={region}
+                        onChange={(val) => setRegion(val)}
+                        classes="ant-select custom-select"
+                      />
+                    </Form.Item>
+                    {formConfig.fields.map((field, index) => (
+                      <Form.Item name={field.label.toLowerCase()} key={index} label={field.label}>
+                        {field.type === "select" && (
+                          <Select defaultValue={field.defaultValue}>
+                            {field.options.map((option, idx) => (
+                              <Select.Option key={idx} value={option}>
+                                {option}
+                              </Select.Option>
+                            ))}
+                          </Select>
+                        )}
                       </Form.Item>
-                    </div>
-                    <Typography style={{ margin: 10, fontSize: 26 }}>{user?.email}</Typography>
-                  </Col>
-                  <Col xs={24} sm={24} md={16} lg={16} xl={16}>
-                    <EditButton type="primary" onClick={showModal}>
-                      Edit Profile
-                    </EditButton>
-                    <Modal title="Edit Information" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} 
-                    footer={[
-                      <Button key="cancel" onClick={handleCancel} style={{ backgroundColor: '#ff7f50', color: 'white' }}>
-                        Cancel
-                      </Button>,
-                      <Button key="submit" type="primary" form="editProfileForm" htmlType="submit">
-                        OK
-                      </Button>
-                    ]}>
-                    <Form layout="vertical" id="editProfileForm" onFinish={onFinish} onFinishFailed={onFinishFailed}>
-                      <Form.Item name="biography" label={formConfig.aboutMe.label}>
-                        <TextArea rows={4} placeholder={formConfig.aboutMe.placeholder} />
-                      </Form.Item>
-                      <Form.Item name="country" label="Country">
-                        <CountryDropdown
-                          value={country}
-                          onChange={(val) => setCountry(val)}
-                          classes="ant-select custom-select"
-                        />
-                      </Form.Item>
-                      <Form.Item name="region" label="Region">
-                        <RegionDropdown
-                          country={country}
-                          value={region}
-                          onChange={(val) => setRegion(val)}
-                          classes="ant-select custom-select"
-                        />
-                      </Form.Item>
-                      {formConfig.fields.map((field, index) => (
-                        <Form.Item name={field.label.toLowerCase()} key={index} label={field.label}>
-                          {field.type === "select" && (
-                            <Select defaultValue={field.defaultValue}>
-                              {field.options.map((option, idx) => (
-                                <Select.Option key={idx} value={option}>
-                                  {option}
-                                </Select.Option>
-                              ))}
-                            </Select>
-                          )}
-                        </Form.Item>
-                      ))}
-                      <Form.Item name="isBlocked" valuePropName="checked" label="Is Blocked">
-                        <Switch />
-                      </Form.Item>
-                      <Form.Item name="isProfilePublic" valuePropName="checked" label="Is Profile Public">
-                        <Switch />
-                      </Form.Item>
-                    </Form>
-                  </Modal>
-                  </Col>
-                </Row>
-                <Divider />
-                <Row justify="center">
-                  <Menu style={{ backgroundColor: "transparent", border: "none", fontSize: "26px" }} mode="horizontal" defaultSelectedKeys={['1']}>
-                    <Menu.Item key="1">Posts</Menu.Item>
-                    <Menu.Item key="2">Information</Menu.Item>
-                    <Menu.Item key="3">Friends</Menu.Item>
-                  </Menu>
-                </Row>
-                <Card style={{ marginTop: "10px", padding: "15px", textAlign: 'center' }}>
-                  <Typography.Title level={4}>Short information</Typography.Title>
-                </Card>
-              </Form>
+                    ))}
+                    <Form.Item name="isBlocked" valuePropName="checked" label="Is Blocked">
+                      <Switch />
+                    </Form.Item>
+                    <Form.Item name="isProfilePublic" valuePropName="checked" label="Is Profile Public">
+                      <Switch />
+                    </Form.Item>
+                  </Form>
+                </Modal>
+              </Col>
+            </Row>
+            <Divider />
+            <Row justify="center">
+              <Menu style={{ backgroundColor: "transparent", border: "none", fontSize: "26px" }} mode="horizontal" defaultSelectedKeys={['1']}>
+                <Menu.Item key="1">Posts</Menu.Item>
+                <Menu.Item key="2">Information</Menu.Item>
+                <Menu.Item key="3">Friends</Menu.Item>
+              </Menu>
+            </Row>
+            <Card style={{ marginTop: "10px", padding: "15px", textAlign: 'center' }}>
+              <Typography.Title level={4}>Short information</Typography.Title>
             </Card>
-          </Row>
-        </div>
-      ) : (
-        <></>
-      )}
-    </>
+          </Form>
+        </Card>
+      </Row>
+    </div>
   );
 };
 
