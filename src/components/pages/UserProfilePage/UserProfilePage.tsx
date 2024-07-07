ad9999c70892b29a0b77395b1f364ff257a3708f
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-07 15:33:10 +0300

    refactor: remove unnacessary form & add initial values 📦

--- src/components/pages/UserProfilePage/UserProfilePage.tsx
+++ src/components/pages/UserProfilePage/UserProfilePage.tsx
@@ -9,13 +9,13 @@ import {
 	Form,
 	Input,
 	Menu,
-	message,
 	Modal,
 	Row,
 	Select,
 	Switch,
 	Typography,
 	Upload,
+	message,
 } from "antd";
 import type React from "react";
 import { useEffect, useState } from "react";
@@ -120,7 +120,7 @@ const UserProfilePage: React.FC = () => {
 			})
 			.catch((error) => {
 				console.error(error);
-        message.error("Failed to change cover photo");
+				message.error("Failed to change cover photo");
 			});
 	};
 
@@ -150,9 +150,9 @@ const UserProfilePage: React.FC = () => {
 				const newAvatar = `/images/avatars/${res.data.userEntity.avatar}`;
 				dispatch(updateAvatar(newAvatar));
 			})
-      .catch((error) => {
-        console.error(error);
-        message.error("Failed to change avatar");
+			.catch((error) => {
+				console.error(error);
+				message.error("Failed to change avatar");
 			});
 	};
 
@@ -169,7 +169,7 @@ const UserProfilePage: React.FC = () => {
 			})
 			.catch((error) => {
 				console.error(error);
-        message.error("Failed to update profile");
+				message.error("Failed to update profile");
 			});
 	};
 
@@ -193,247 +193,221 @@ const UserProfilePage: React.FC = () => {
 						boxShadow: "none",
 					}}
 				>
-					<Form
-						name="userProfile"
-						onFinish={onFinish}
-						onFinishFailed={onFinishFailed}
-						requiredMark={false}
+					<div
+						className="box"
+						style={{
+							backgroundColor: "#D9D9D9",
+							height: "40vh",
+							borderRadius: "10px",
+							backgroundImage: `url(${coverPhoto})`,
+							backgroundSize: "cover",
+							backgroundPosition: "center",
+							position: "relative",
+						}}
 					>
-						<Form.Item
-							name="coverPhoto"
-							valuePropName="file"
-							getValueFromEvent={(e: UploadChangeParam) => {
-								const image = e?.fileList[0] as IUploadedFile;
-								return image?.originFileObj;
-							}}
-						>
-							<div
-								className="box"
-								style={{
-									backgroundColor: "#D9D9D9",
-									height: "40vh",
-									borderRadius: "10px",
-									backgroundImage: `url(${coverPhoto})`,
-									backgroundSize: "cover",
-									backgroundPosition: "center",
-									position: "relative",
-								}}
+						{coverPhoto === editImg ? (
+							<Upload
+								showUploadList={false}
+								beforeUpload={() => false}
+								accept="image/*"
+								onChange={handleCoverPhotoChange}
+								maxCount={1}
+								defaultFileList={[]}
 							>
-								{coverPhoto === editImg ? (
-									<Upload
-										showUploadList={false}
-										beforeUpload={() => false}
-										accept="image/*"
-										onChange={handleCoverPhotoChange}
-										maxCount={1}
-										defaultFileList={[]}
-									>
-										<CoverButton
-											style={{
-												display: "flex",
-												alignItems: "center",
-												border: "none",
-												right: "0px",
-												bottom: "0px",
-												position: "absolute",
-											}}
-										>
-											<img
-												src={cameraImg}
-												alt="coverPhoto"
-												style={{ width: 26, height: 22, margin: 5 }}
-											/>
-											Add cover photo
-										</CoverButton>
-									</Upload>
-								) : (
-									<Dropdown
-										overlay={
-											<CoverPhotoMenu
-												handleCoverPhotoChange={handleCoverPhotoChange}
-												setCoverPhoto={setCoverPhoto}
-												userCoverPhoto={userProfile?.coverPhoto}
-											/>
-										}
-										trigger={["click"]}
-									>
-										<CoverButton
-											style={{
-												display: "flex",
-												alignItems: "center",
-												border: "none",
-												right: "0px",
-												bottom: "0px",
-												position: "absolute",
-											}}
-										>
-											<img
-												src={editImg}
-												alt="editCoverPhoto"
-												style={{ width: 26, height: 22, margin: 5 }}
-											/>
-											Edit cover photo
-										</CoverButton>
-									</Dropdown>
-								)}
-							</div>
-						</Form.Item>
-						<Row
-							style={{
-								marginTop: "-7%",
-								display: "flex",
-								alignItems: "center",
-								justifyContent: "space-between",
-							}}
-						>
-							<Col
-								xs={24}
-								sm={24}
-								md={8}
-								lg={8}
-								xl={8}
-								style={{ marginBottom: "1rem" }}
-							>
-								<div>
-									<Form.Item
-										name="avatar"
-										valuePropName="file"
-										getValueFromEvent={(e: UploadChangeParam) => {
-											const image = e?.fileList[0] as IUploadedFile;
-											return image?.originFileObj;
-										}}
-									>
-										<Avatar size={160} src={avatar} />
-										<Dropdown
-											overlay={
-												<AvatarMenu
-													handleAvatarChange={handleAvatarChange}
-													setAvatar={setAvatar}
-												/>
-											}
-											trigger={["click"]}
-										>
-											<AvatarButton
-												style={{
-													border: "none",
-													color: "black",
-													borderRadius: "100px",
-												}}
-												icon={<CameraOutlined />}
-											/>
-										</Dropdown>
-									</Form.Item>
-								</div>
-								<Typography style={{ margin: 10, fontSize: 26 }}>
-									{user?.email}
-								</Typography>
-							</Col>
-							<Col xs={24} sm={24} md={16} lg={16} xl={16}>
-								<EditButton type="primary" onClick={showModal}>
-									Edit Profile
-								</EditButton>
-								<Modal
-									title="Edit Information"
-									visible={isModalVisible}
-									onOk={handleOk}
-									onCancel={handleCancel}
-									footer={[
-										<Button
-											key="cancel"
-											onClick={handleCancel}
-											style={{ backgroundColor: "#ff7f50", color: "white" }}
-										>
-											Cancel
-										</Button>,
-										<Button
-											key="submit"
-											type="primary"
-											form="editProfileForm"
-											htmlType="submit"
-										>
-											OK
-										</Button>,
-									]}
+								<CoverButton
+									style={{
+										display: "flex",
+										alignItems: "center",
+										border: "none",
+										right: "0px",
+										bottom: "0px",
+										position: "absolute",
+									}}
 								>
-									<Form
-										layout="vertical"
-										id="editProfileForm"
-										onFinish={onFinish}
-										onFinishFailed={onFinishFailed}
-									>
-										<Form.Item name="userId" hidden initialValue={user?.id} />
-
-										<Form.Item name="biography" label="About Me">
-											<Input.TextArea
-												rows={4}
-												placeholder="Tell about yourself..."
-											/>
-										</Form.Item>
-										<Form.Item name="country" label="Country">
-											<CountryDropdown
-												value={country}
-												onChange={(val) => setCountry(val)}
-												classes="ant-select custom-select"
-											/>
-										</Form.Item>
-										<Form.Item name="region" label="Region">
-											<RegionDropdown
-												country={country}
-												value={region}
-												onChange={(val) => setRegion(val)}
-												classes="ant-select custom-select"
-											/>
-										</Form.Item>
-										<Form.Item name="pronouns" label="Pronouns">
-											<Select
-												defaultValue="do not specify"
-												options={PronounsOptions}
-											/>
-										</Form.Item>
-										<Form.Item
-											name="isBlocked"
-											valuePropName="checked"
-											label="Is Blocked"
-										>
-											<Switch />
-										</Form.Item>
-										<Form.Item
-											name="isProfilePublic"
-											valuePropName="checked"
-											label="Is Profile Public"
-										>
-											<Switch />
-										</Form.Item>
-									</Form>
-								</Modal>
-							</Col>
-						</Row>
-						<Divider />
-						<Row justify="center">
-							<Menu
-								style={{
-									backgroundColor: "transparent",
-									border: "none",
-									fontSize: "26px",
-								}}
-								mode="horizontal"
-								defaultSelectedKeys={["1"]}
+									<img
+										src={cameraImg}
+										alt="coverPhoto"
+										style={{ width: 26, height: 22, margin: 5 }}
+									/>
+									Add cover photo
+								</CoverButton>
+							</Upload>
+						) : (
+							<Dropdown
+								overlay={
+									<CoverPhotoMenu
+										handleCoverPhotoChange={handleCoverPhotoChange}
+										setCoverPhoto={setCoverPhoto}
+										userCoverPhoto={userProfile?.coverPhoto}
+									/>
+								}
+								trigger={["click"]}
 							>
-								<Menu.Item key="1">Posts</Menu.Item>
-								<Menu.Item key="2">Information</Menu.Item>
-								<Menu.Item key="3">Friends</Menu.Item>
-							</Menu>
-						</Row>
-						<Card
-							style={{
-								marginTop: "10px",
-								padding: "15px",
-								textAlign: "center",
-							}}
+								<CoverButton
+									style={{
+										display: "flex",
+										alignItems: "center",
+										border: "none",
+										right: "0px",
+										bottom: "0px",
+										position: "absolute",
+									}}
+								>
+									<img
+										src={editImg}
+										alt="editCoverPhoto"
+										style={{ width: 26, height: 22, margin: 5 }}
+									/>
+									Edit cover photo
+								</CoverButton>
+							</Dropdown>
+						)}
+					</div>
+					<Row
+						style={{
+							marginTop: "-7%",
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "space-between",
+						}}
+					>
+						<Col
+							xs={24}
+							sm={24}
+							md={8}
+							lg={8}
+							xl={8}
+							style={{ marginBottom: "1rem" }}
 						>
-							<Typography.Title level={4}>Short information</Typography.Title>
-						</Card>
-					</Form>
+							<Avatar size={160} src={avatar} />
+							<Dropdown
+								overlay={
+									<AvatarMenu
+										handleAvatarChange={handleAvatarChange}
+										setAvatar={setAvatar}
+									/>
+								}
+								trigger={["click"]}
+							>
+								<AvatarButton
+									style={{
+										border: "none",
+										color: "black",
+										borderRadius: "100px",
+									}}
+									icon={<CameraOutlined />}
+								/>
+							</Dropdown>
+							<Typography style={{ margin: 10, fontSize: 26 }}>
+								{user?.email}
+							</Typography>
+						</Col>
+						<Col xs={24} sm={24} md={16} lg={16} xl={16}>
+							<EditButton type="primary" onClick={showModal}>
+								Edit Profile
+							</EditButton>
+							<Modal
+								title="Edit Information"
+								visible={isModalVisible}
+								onOk={handleOk}
+								onCancel={handleCancel}
+								footer={[
+									<Button
+										key="cancel"
+										onClick={handleCancel}
+										style={{ backgroundColor: "#ff7f50", color: "white" }}
+									>
+										Cancel
+									</Button>,
+									<Button
+										key="submit"
+										type="primary"
+										form="editProfileForm"
+										htmlType="submit"
+									>
+										OK
+									</Button>,
+								]}
+							>
+								<Form
+									layout="vertical"
+									id="editProfileForm"
+									onFinish={onFinish}
+									onFinishFailed={onFinishFailed}
+									initialValues={userProfile ? userProfile : []}
+								>
+									<Form.Item name="userId" hidden initialValue={user?.id} />
+
+									<Form.Item name="biography" label="About Me">
+										<Input.TextArea
+											rows={4}
+											placeholder="Tell about yourself..."
+										/>
+									</Form.Item>
+									<Form.Item name="country" label="Country">
+										<CountryDropdown
+											value={country}
+											onChange={(val) => setCountry(val)}
+											classes="ant-select custom-select"
+										/>
+									</Form.Item>
+									<Form.Item name="region" label="Region">
+										<RegionDropdown
+											country={country}
+											value={region}
+											onChange={(val) => setRegion(val)}
+											classes="ant-select custom-select"
+										/>
+									</Form.Item>
+									<Form.Item name="pronouns" label="Pronouns">
+										<Select
+											defaultValue="do not specify"
+											options={PronounsOptions}
+										/>
+									</Form.Item>
+									<Form.Item
+										name="isBlocked"
+										valuePropName="checked"
+										label="Is Blocked"
+									>
+										<Switch />
+									</Form.Item>
+									<Form.Item
+										name="isProfilePublic"
+										valuePropName="checked"
+										label="Is Profile Public"
+									>
+										<Switch />
+									</Form.Item>
+								</Form>
+							</Modal>
+						</Col>
+					</Row>
+					<Divider />
+					<Row justify="center">
+						<Menu
+							style={{
+								backgroundColor: "transparent",
+								border: "none",
+								fontSize: "26px",
+							}}
+							mode="horizontal"
+							defaultSelectedKeys={["1"]}
+						>
+							<Menu.Item key="1">Posts</Menu.Item>
+							<Menu.Item key="2">Information</Menu.Item>
+							<Menu.Item key="3">Friends</Menu.Item>
+						</Menu>
+					</Row>
+					<Card
+						style={{
+							marginTop: "10px",
+							padding: "15px",
+							textAlign: "center",
+						}}
+					>
+						<Typography.Title level={4}>Short information</Typography.Title>
+					</Card>
 				</Card>
 			</Row>
 		</div>
