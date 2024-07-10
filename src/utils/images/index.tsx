a3493de55eefc37f53355b0ec83a0e1ff31cc505
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-07-10 15:02:15 +0300

    Committing changes before pulling

--- src/utils/images/index.tsx
+++ src/utils/images/index.tsx
@@ -15,7 +15,7 @@ import avatar from '../../assets/authentication/avatar.png';
 import forgotPasswordImage from '../../assets/authentication/forgot_password_image.png'
 import confirmedMail from '../../assets/authentication/confirmemail.png'
 import changeEmail from '../../assets/authentication/change_email.png'
-import logo from '../../assets/authentication/logotype.png';
+import logo from '../../assets/logotype.png';
 import home from '../../assets/profile/home.png';
 import activeHome from '../../assets/profile/active_home.png';
 import friends from '../../assets/profile/friends.png';
@@ -28,8 +28,10 @@ import glyph from '../../assets/profile/glyph.png';
 import minusImg from '../../assets/post/minus.png';
 import hoverMinusImg from '../../assets/post/hover_minus.png';
 import ActiveMinusImg from '../../assets/post/active_minus.png';
-import cameraImg from '../../assets/profile/camera.png'
-import editImg from '../../assets/profile/edit.png'
+import cameraImg from '../../assets/profile/camera.png';
+import editImg from '../../assets/profile/edit.png';
+import lateAtNight from '../../assets/authentication/late_at_night.png';
+import reactAmico from '../../assets/authentication/react_amico.png';
 
 //Backgrounds
 import bg1 from '../../assets/backgrounds/1.jpg';
@@ -42,44 +44,46 @@ import bg7 from '../../assets/backgrounds/7.jpg';
 import bg8 from '../../assets/backgrounds/8.jpg';
 
 export {
-    photoImg,
-    locationImg,
-    planetImg,
-    tagImg,
-    friendsImg,
-    friendsExceptImg,
-    postTypeImg,
-    userImg,
-    imageStoryIcon,
-    textStoryIcon,
-    settingsIcon,
-    setNewPasswordImg,
-    resetPasswordImg,
-    avatar,
-    forgotPasswordImage,
-    confirmedMail,
-    changeEmail,
-    logo,
-    home,
-    activeHome,
-    friends,
-    activeFriends,
-    story,
-    activeStory,
-    messanger,
-    activeMessanger,
-    glyph,
-	minusImg,
-	hoverMinusImg,
-	ActiveMinusImg,
-    cameraImg,
-    editImg,
-    bg1,
-    bg2,
-    bg3,
-    bg4,
-    bg5,
-    bg6,
-    bg7,
-    bg8
+   photoImg,
+   locationImg,
+   planetImg,
+   tagImg,
+   friendsImg,
+   friendsExceptImg,
+   postTypeImg,
+   userImg,
+   imageStoryIcon,
+   textStoryIcon,
+   settingsIcon,
+   setNewPasswordImg,
+   resetPasswordImg,
+   avatar,
+   forgotPasswordImage,
+   confirmedMail,
+   changeEmail,
+   logo,
+   home,
+   activeHome,
+   friends,
+   activeFriends,
+   story,
+   activeStory,
+   messanger,
+   activeMessanger,
+   glyph,
+   minusImg,
+   hoverMinusImg,
+   ActiveMinusImg,
+   cameraImg,
+   editImg,
+   lateAtNight,
+   reactAmico,
+   bg1,
+   bg2,
+   bg3,
+   bg4,
+   bg5,
+   bg6,
+   bg7,
+   bg8
 };
