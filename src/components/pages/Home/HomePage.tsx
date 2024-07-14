0450b8a9c704e154eb85e041f81b9ea08da531af
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-07-14 16:39:25 +0300

    feat: render PostForm and StoryForm based on user authentication

--- src/components/pages/Home/HomePage.tsx
+++ src/components/pages/Home/HomePage.tsx
@@ -4,19 +4,26 @@ import StoryForm from './components/StoryForm';
 import PostForm from './components/PostForm';
 import './HomePage.css';
 import CreatePostButton from '../../post/CreatePostButton';
+import { useAppSelector } from '../../../hooks/redux';
 
 const { Content } = Layout;
 
 export const HomePage = () => {
+   const { isLogin } = useAppSelector(state => state.account);
+
    return (
       <Layout>
          <SideBar />
          <Layout style={{ marginLeft: 250 }}>
             <Content style={{ padding: '0 50px', marginTop: 64 }}>
                <div className="homepage-container">
-                  <CreatePostButton/>
-                  <StoryForm />
-                  <PostForm />
+                  {isLogin ? (
+                     <>
+                        <CreatePostButton />
+                        <StoryForm />
+                        <PostForm />
+                     </>
+                  ) : null}
                </div>
             </Content>
          </Layout>
