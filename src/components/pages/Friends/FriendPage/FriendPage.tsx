b697885ec4802f1190692d2394a9263d7ed0bd7d
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-23 11:36:39 +0300

    refactor: separate FriendRecommendationCard component 📦

--- src/components/pages/Friends/FriendPage/FriendPage.tsx
+++ src/components/pages/Friends/FriendPage/FriendPage.tsx
@@ -1,14 +1,12 @@
-import { Card, Col, Row, Button, message } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Row, message } from "antd";
 import { useEffect, useState } from "react";
 import { apiClient } from "../../../../utils/api/apiClient";
-import type { IFriendRecommendation, ISendFriendRequest } from "./types";
+import type { IFriendRecommendation } from "./types";
 import { avatar } from "../../../../utils/images";
 import { APP_ENV } from "../../../../env";
-import { useAppSelector } from "../../../../hooks/redux";
+import FriendRecommendationCard from "./components/FriendRecommendationCard";
 
 const FriendPage = () => {
-	const { user } = useAppSelector((state) => state.account);
 	const [friends, setFriends] = useState<IFriendRecommendation[]>([]);
 
 	useEffect(() => {
@@ -31,53 +29,12 @@ const FriendPage = () => {
 			});
 	}, []);
 
-	const sendFriendRequest = (friendId: string) => {
-		if (user?.id === undefined) {
-			message.error("Send friend request error");
-			return;
-		}
-
-		const sendFriendRequestBody: ISendFriendRequest = {
-			friendId,
-			userId: user?.id,
-		};
-
-		apiClient
-			.post("/api/friends/send-friend-request", sendFriendRequestBody)
-			.then(() => {
-				message.success("Request successfully sended!");
-			})
-			.catch(() => {
-				message.error("Request sending error");
-			});
-	};
-
 	return (
 		<div style={{ padding: "20px" }}>
 			<h2>People you may know</h2>
 			<Row gutter={[16, 16]}>
 				{friends.map((friend) => (
-					<Col span={6} key={friend.id}>
-						<Card
-							cover={
-								<img alt="Friend recommendation avatar" src={friend.avatar} />
-							}
-							actions={[
-								<Button
-									type="primary"
-									key="add"
-									onClick={() => sendFriendRequest(friend.id)}
-								>
-									Add friend
-								</Button>,
-							]}
-						>
-							<Card.Meta
-								avatar={<UserOutlined />}
-								title={`${friend.lastName} ${friend.firstName}`}
-							/>
-						</Card>
-					</Col>
+					<FriendRecommendationCard key={friend.id} friend={friend}/>
 				))}
 			</Row>
 		</div>
---
+++ src/components/pages/Friends/FriendPage/components/FriendRecommendationCard.tsx
@@ -0,0 +1,60 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Col, Card, Button, message } from "antd";
+import type { IFriendRecommendation, ISendFriendRequest } from "../types";
+import { useAppSelector } from "../../../../../hooks/redux";
+import { apiClient } from "../../../../../utils/api/apiClient";
+
+type FriendRecommendationCardProps = {
+	friend: IFriendRecommendation;
+};
+
+const FriendRecommendationCard = ({
+	friend,
+}: FriendRecommendationCardProps) => {
+	const { user } = useAppSelector((state) => state.account);
+
+	const sendFriendRequest = (friendId: string) => {
+		if (user?.id === undefined) {
+			message.error("Send friend request error");
+			return;
+		}
+
+		const sendFriendRequestBody: ISendFriendRequest = {
+			friendId,
+			userId: user?.id,
+		};
+
+		apiClient
+			.post("/api/friends/send-friend-request", sendFriendRequestBody)
+			.then(() => {
+				message.success("Request successfully sended!");
+			})
+			.catch(() => {
+				message.error("Request sending error");
+			});
+	};
+
+	return (
+		<Col span={6} key={friend.id}>
+			<Card
+				cover={<img alt="Friend recommendation avatar" src={friend.avatar} />}
+				actions={[
+					<Button
+						type="primary"
+						key="add"
+						onClick={() => sendFriendRequest(friend.id)}
+					>
+						Add friend
+					</Button>,
+				]}
+			>
+				<Card.Meta
+					avatar={<UserOutlined />}
+					title={`${friend.lastName} ${friend.firstName}`}
+				/>
+			</Card>
+		</Col>
+	);
+};
+
+export default FriendRecommendationCard;
