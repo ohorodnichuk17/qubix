cd8bfcbfda13da0d370b89e94a2882525313e078
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-25 12:56:21 +0300

    refactor: separate PostItemCard component 📦

---
+++ src/components/post/list/components/PostItemCard.tsx
@@ -0,0 +1,119 @@
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { Card, Flex, Avatar, Divider, Carousel, Tag } from "antd";
+import { APP_ENV } from "../../../../env";
+import { avatar, locationImg } from "../../../../utils/images";
+import { getRandomTagColor } from "../../create/components/Tags/TagsList";
+import type { IPost } from "../types";
+import { ACTION_OPTIONS, FEELING_OPTIONS } from "../../../feelings/constants";
+import type { IAction, ISubAction, IFeeling } from "../../../feelings/types";
+
+type PostItemCardProps = {
+	post: IPost;
+};
+
+const PostItemCard = ({ post }: PostItemCardProps) => {
+	const getPublicationDate = (date: string) => new Date(date).toDateString();
+
+	const getActionImage = (action: IAction, subAction: ISubAction) =>
+		subAction
+			? ACTION_OPTIONS.find((a) => a.name === action.name)?.subActions?.find(
+					(s) => s.name === subAction.name,
+				)?.emoji
+			: ACTION_OPTIONS.find((a) => a.name === action.name)?.emoji;
+
+	const getFeelingImage = (feeling: IFeeling) =>
+		FEELING_OPTIONS.find((f) => f.name === feeling.name)?.emoji;
+
+	return (
+		<Card key={post.id} style={{ width: "100%" }}>
+			<Flex vertical gap="small">
+				<Flex align="center" gap="small">
+					<Avatar
+						size={60}
+						src={
+							post.user.avatar === null
+								? avatar
+								: `${APP_ENV.BASE_URL}/images/avatars/${post.user.avatar}`
+						}
+					/>
+					<Flex vertical>
+						<span
+							style={{ fontWeight: 600, fontSize: 20 }}
+						>{`${post.user.firstName} ${post.user.lastName}`}</span>
+						<span>{getPublicationDate(post.createdAt)}</span>
+					</Flex>
+				</Flex>
+
+				<Divider style={{ margin: 0 }} />
+
+				{post.action && (
+					<Flex align="center" gap="small">
+						<img
+							src={getActionImage(post.action, post.subAction)}
+							alt="Action icon"
+							style={{ height: 35, width: 35 }}
+						/>
+						<span>{post.action.name}</span>
+
+						{post.subAction && <span>{post.subAction.name}</span>}
+					</Flex>
+				)}
+
+				{post.feeling && (
+					<Flex align="center" gap="small">
+						<img
+							src={getFeelingImage(post.feeling)}
+							alt="Feeling icon"
+							style={{ height: 35, width: 35 }}
+						/>
+						<span>{post.feeling.name}</span>
+					</Flex>
+				)}
+
+				<Carousel
+					arrows
+					draggable
+					infinite
+					prevArrow={<LeftOutlined />}
+					nextArrow={<RightOutlined />}
+				>
+					{post.images.map((image) => (
+						<img
+							key={image.id}
+							src={`${APP_ENV.BASE_URL}/images/posts/${image.imagePath}`}
+							className="post-preview-img"
+							alt="Post images"
+						/>
+					))}
+				</Carousel>
+
+				{post.location && (
+					<Flex align="center" gap="small">
+						<img src={locationImg} height={35} alt="Post images" />
+						<span>{post.location}</span>
+					</Flex>
+				)}
+
+				{post.content !== "undefined" && (
+					<p style={{ margin: 0 }}>{post.content}</p>
+				)}
+
+				{post.tags && (
+					<Flex wrap="wrap" gap="small">
+						{post.tags.map((tag) => (
+							<Tag
+								key={tag}
+								color={getRandomTagColor()}
+								style={{ margin: 0, width: "fit-content" }}
+							>
+								{`#${tag}`}
+							</Tag>
+						))}
+					</Flex>
+				)}
+			</Flex>
+		</Card>
+	);
+};
+
+export default PostItemCard;
