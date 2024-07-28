e462b765e86036ae7f8768c42a72c62168f77b8d
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-28 09:58:03 +0300

    feat: FriendRequestsPage ✨

---
+++ src/components/pages/Friends/FriendPage/components/FriendRequestCard.tsx
@@ -0,0 +1,89 @@
+import { UserOutlined } from "@ant-design/icons";
+import { Col, Card, Button, message } from "antd";
+import { useNavigate } from "react-router-dom";
+import { apiClient } from "../../../../../utils/api/apiClient";
+import { useState } from "react";
+import { useAppSelector } from "../../../../../hooks/redux";
+import type { IFriendRecommendation } from "../types";
+
+type FriendRequestCardProps = {
+	friend: IFriendRecommendation;
+	removeAcceptedRequestFriend: (
+		acceptedRequestFriend: IFriendRecommendation,
+	) => void;
+};
+
+const FriendRequestCard = ({
+	friend,
+	removeAcceptedRequestFriend,
+}: FriendRequestCardProps) => {
+	const navigate = useNavigate();
+
+	const { user } = useAppSelector((state) => state.account);
+
+	const [loading, setLoading] = useState<boolean>(false);
+
+	const acceptFriendRequest = (e: React.MouseEvent) => {
+		e.stopPropagation();
+
+		const values = {
+			userId: user?.id,
+			friendId: friend.id,
+		};
+
+		setLoading(true);
+
+		apiClient
+			.post("/api/friends/accept-friend-request", values)
+			.then(() => {
+				message.success("Friend Request accepted!");
+				removeAcceptedRequestFriend(friend);
+			})
+			.catch(() => {
+				message.error("Friend request accepting error!");
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	};
+
+	return (
+		<Col
+			style={{ minWidth: 250 }}
+			xs={{ flex: "100%" }}
+			sm={{ flex: "50%" }}
+			md={{ flex: "40%" }}
+			lg={{ flex: "20%" }}
+			xl={{ flex: "10%" }}
+		>
+			<Card
+				style={{ cursor: "pointer" }}
+				onClick={() => navigate(`/profile?userId=${friend.id}`)}
+				cover={
+					<img
+						style={{ height: 200, objectFit: "cover" }}
+						alt="Friend recommendation avatar"
+						src={friend.avatar}
+					/>
+				}
+				actions={[
+					<Button
+						type="primary"
+						key="add"
+						loading={loading}
+						onClick={acceptFriendRequest}
+					>
+						Accept friend request
+					</Button>,
+				]}
+			>
+				<Card.Meta
+					avatar={<UserOutlined />}
+					title={`${friend.lastName} ${friend.firstName}`}
+				/>
+			</Card>
+		</Col>
+	);
+};
+
+export default FriendRequestCard;
--- src/components/pages/Friends/FriendRequest/FriendRequest.tsx
+++ src/components/pages/Friends/FriendRequest/FriendRequest.tsx
@@ -1,72 +1,75 @@
-import { Button, Grid, Layout, message, Typography } from "antd";
+import { Grid, Layout, Row, Typography } from "antd";
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
 import { useAppSelector } from "../../../../hooks/redux";
 import { apiClient } from "../../../../utils/api/apiClient";
 import FriendSidebar from "../FriendPage/FriendSidebar";
 import { UserAddOutlined } from "@ant-design/icons";
+import FriendRequestCard from "../FriendPage/components/FriendRequestCard";
+import type { IFriendRecommendation } from "../FriendPage/types";
+import { avatar } from "../../../../utils/images";
+import { APP_ENV } from "../../../../env";
 
 const { Content } = Layout;
 const { useBreakpoint } = Grid;
 const { Title } = Typography;
 
 const FriendRequest = () => {
-   const navigate = useNavigate();
-   const [searchParams] = useSearchParams();
-   const friendId = searchParams.get("friendId");
-   const screens = useBreakpoint();
+	const screens = useBreakpoint();
 
-   const { user } = useAppSelector((state) => state.account);
-   const [friend, setFriend] = useState(null);
+	const { user } = useAppSelector((state) => state.account);
+	const [friends, setFriends] = useState<IFriendRecommendation[]>([]);
 
-   useEffect(() => {
-      apiClient
-         .get(
-            `/api/friends/get-friend-by-id?userId=${user?.id}&friendId=${friendId}`,
-         )
-         .then((res) => {
-            setFriend(res.data);
-         });
-   }, [user?.id, friendId]);
+	useEffect(() => {
+		apiClient.get(`/api/friends/requests?userId=${user?.id}`).then((res) => {
+			const updatedFriends = res.data.map((friend: IFriendRecommendation) => ({
+				...friend,
+				avatar:
+					friend.avatar === null
+						? avatar
+						: `${APP_ENV.BASE_URL}/images/avatars/${friend.avatar}`,
+			}));
+			setFriends(updatedFriends);
+		});
+	}, [user?.id]);
 
-   const acceptFriendRequest = () => {
-      const values = {
-         userId: user?.id,
-         friendId: friendId,
-      };
+const removeAcceptedRequestFriend = (
+	acceptedRequestFriend: IFriendRecommendation,
+) =>
+	setFriends(friends.filter((friend) => friend.id !== acceptedRequestFriend.id));
 
-      apiClient
-         .post("/api/friends/accept-friend-request", values)
-         .then(() => {
-            message.success("Friend Request accepted!");
-            navigate("/");
-         })
-         .catch(() => {
-            message.error("Friend request accepting error!");
-         });
-   };
-
-   return (
-      // <Layout style={{ marginLeft: screens.xs ? 80 : 256, transition: 'margin-left 0.2s' }}>
-      //    <FriendSidebar></FriendSidebar>
-      //    <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
-      //       <p>{friendId}</p>
-      //       <p>{friend?.Email}</p>
-      //       <p>{friend?.UserName}</p>
-
-      //       <Button onClick={acceptFriendRequest}>Accept request</Button>
-      //    </Content>
-      // </Layout>      
-      <div>
-         <FriendSidebar select="2"></FriendSidebar>
-         <Layout style={{ marginLeft: screens.xs ? 80 : 256, transition: 'margin-left 0.2s' }}>
-            <Content style={{ margin: '24px 16px 0', overflow: 'initial', textAlign: "center" }}>
-               <UserAddOutlined style={{ fontSize: '64px' }} />
-               <Title level={3}>Friend requests will be shown here</Title>
-            </Content>
-         </Layout>
-      </div>
-   );
+	return (
+		<div>
+			<FriendSidebar select="2" />
+			<Layout
+				style={{
+					marginLeft: screens.xs ? 80 : 256,
+					transition: "margin-left 0.2s",
+				}}
+			>
+				<Content
+					style={{
+						margin: "24px 16px 0",
+						overflow: "initial",
+						textAlign: "center",
+					}}
+				>
+					{friends.length === 0 && (
+						<>
+							<UserAddOutlined style={{ fontSize: "64px" }} />
+							<Title level={3}>Friend requests will be shown here</Title>
+						</>
+					)}
+					{friends.length > 0 && (
+						<Row gutter={[16, 16]} wrap={true}>
+							{friends.map((friend) => (
+								<FriendRequestCard key={friend.id} friend={friend} removeAcceptedRequestFriend={removeAcceptedRequestFriend}/>
+							))}
+						</Row>
+					)}
+				</Content>
+			</Layout>
+		</div>
+	);
 };
 
 export default FriendRequest;
