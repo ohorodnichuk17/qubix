858ba93b217dd115e1022c72c364bbcdfdc62396
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-02 10:05:59 +0300

    feat: ad navigation to home after story creation success

--- src/components/pages/Story/CreateStoryPage/CreateStoryPage.tsx
+++ src/components/pages/Story/CreateStoryPage/CreateStoryPage.tsx
@@ -8,79 +8,81 @@ import StoryPreview from "./components/StoryPreview";
 import StorySettingsCard from "./components/StorySettingsCard";
 import { CreateStoryProvider, useCreateStory } from "./context";
 import useCapture from "./hooks/useCapture";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 const CreateStoryContent = () => {
-	const { storyType, setStoryType, text } = useCreateStory();
+   const { storyType, setStoryType, text } = useCreateStory();
+   const { captureAreaRef, getCapture } = useCapture();
+   const navigate = useNavigate(); // Initialize useNavigate
 
-	const { captureAreaRef, getCapture } = useCapture();
+   const postStory = async () => {
+      if (storyType == null) return;
+      const story = await getCapture();
 
-	const postStory = async () => {
-		if (storyType == null) return;
-		const story = await getCapture();
+      const formData = new FormData();
+      formData.append("Content", text ?? "");
+      formData.append("Image", story as Blob);
 
-		const formData = new FormData();
-		formData.append("Content", text ?? "");
-		formData.append("Image", story as Blob);
+      apiClient
+         .post("/api/story/create", formData)
+         .then((res) => {
+            console.log(res);
+            message.success("Story successfully posted!");
+            navigate("/");
+         })
+         .catch((error) => {
+            console.log(error);
+            message.error("Post story error!");
+         });
+   };
 
-		apiClient
-			.post("/api/story/create", formData)
-			.then((res) => {
-				console.log(res);
-				message.success("Story successfully posted!");
-			})
-			.catch((error) => {
-				console.log(error);
-				message.error("Post story error!");
-			});
-	};
+   const screens = Grid.useBreakpoint();
 
-	const screens = Grid.useBreakpoint();
+   const isScreenSmallerThatMd =
+      (screens.xs || screens.sm) &&
+      !screens.md &&
+      !screens.lg &&
+      !screens.xl &&
+      !screens.xxl;
 
-	const isScreenSmallerThatMd =
-		(screens.xs || screens.sm) &&
-		!screens.md &&
-		!screens.lg &&
-		!screens.xl &&
-		!screens.xxl;
+   return (
+      <Layout className="create-page-story-layout" style={{ height: "100%" }}>
+         {!isScreenSmallerThatMd && <CreateStorySideBar postStory={postStory} />}
+         <Content
+            style={{
+               marginLeft: !isScreenSmallerThatMd ? "35%" : 0,
+            }}
+            className="content"
+         >
+            {isScreenSmallerThatMd && (
+               <StorySettingsCard
+                  isSmallerThatMdScreen={isScreenSmallerThatMd}
+                  postStory={postStory}
+               />
+            )}
+            <Flex
+               gap="middle"
+               className="create-story-page"
+               style={{
+                  height: isScreenSmallerThatMd ? "fit-content" : "100%",
+                  marginTop: isScreenSmallerThatMd ? 20 : 0,
+               }}
+            >
+               {storyType == null && <SelectStoryType setStoryType={setStoryType} />}
 
-	return (
-		<Layout className="create-page-story-layout" style={{ height: "100%" }}>
-			{!isScreenSmallerThatMd && <CreateStorySideBar postStory={postStory} />}
-			<Content
-				style={{
-					marginLeft: !isScreenSmallerThatMd ? "35%" : 0,
-				}}
-				className="content"
-			>
-				{isScreenSmallerThatMd && (
-					<StorySettingsCard
-						isSmallerThatMdScreen={isScreenSmallerThatMd}
-						postStory={postStory}
-					/>
-				)}
-				<Flex
-					gap="middle"
-					className="create-story-page"
-					style={{
-						height: isScreenSmallerThatMd ? "fit-content" : "100%",
-						marginTop: isScreenSmallerThatMd ? 20 : 0,
-					}}
-				>
-					{storyType == null && <SelectStoryType setStoryType={setStoryType} />}
-
-					{storyType != null && (
-						<StoryPreview captureAreaRef={captureAreaRef} />
-					)}
-				</Flex>
-			</Content>
-		</Layout>
-	);
+               {storyType != null && (
+                  <StoryPreview captureAreaRef={captureAreaRef} />
+               )}
+            </Flex>
+         </Content>
+      </Layout>
+   );
 };
 
 const CreateStoryPage = () => (
-	<CreateStoryProvider>
-		<CreateStoryContent />
-	</CreateStoryProvider>
+   <CreateStoryProvider>
+      <CreateStoryContent />
+   </CreateStoryProvider>
 );
 
 export default CreateStoryPage;
