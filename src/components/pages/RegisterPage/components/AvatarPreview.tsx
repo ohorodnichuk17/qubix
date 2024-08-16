9a14ae36a1a558f6e816cccbc5c83334a9b7cd35
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-16 20:08:08 +0300

    Split registration page into modular components

---
+++ src/components/pages/RegisterPage/components/AvatarPreview.tsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import { Upload, Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+import { UploadChangeParam, UploadFile } from 'antd/es/upload/interface';
+import { getBase64 } from '../../../../utils/helpers/getBase64';
+import { FileType } from '../../../../types/FileType';
+
+interface AvatarPreviewProps {
+   initialImage: string;
+   onImageChange: (image: string) => void;
+}
+
+const AvatarPreview: React.FC<AvatarPreviewProps> = ({ initialImage, onImageChange }) => {
+   const [previewImage, setPreviewImage] = useState(initialImage);
+
+   useEffect(() => {
+      setPreviewImage(initialImage);
+   }, [initialImage]);
+
+   const handleAvatarChange = async (info: UploadChangeParam) => {
+      const file = info.fileList[0];
+      if (file && !file.url && !file.preview) {
+         file.preview = await getBase64(file.originFileObj as FileType);
+      }
+      const image = file?.url || (file?.preview as string);
+      if (image) {
+         setPreviewImage(image);
+         onImageChange(image);
+      }
+   };
+
+   const handlePreview = async (file: UploadFile) => {
+      if (!file.url && !file.preview) {
+         file.preview = await getBase64(file.originFileObj as FileType);
+      }
+      setPreviewImage(file.url || (file.preview as string));
+   };
+
+   return (
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+         {previewImage && (
+            <img
+               src={previewImage}
+               alt="Avatar"
+               style={{ width: '150px', height: '150px', borderRadius: '50%', objectFit: 'cover', marginBottom: '10px' }}
+            />
+         )}
+         <Upload
+            showUploadList={false}
+            beforeUpload={() => false}
+            accept="image/*"
+            onChange={handleAvatarChange}
+            onPreview={handlePreview}
+            maxCount={1}
+         >
+            <Button icon={<UploadOutlined />}>Upload Image</Button>
+         </Upload>
+      </div>
+   );
+};
+
+export default AvatarPreview;
