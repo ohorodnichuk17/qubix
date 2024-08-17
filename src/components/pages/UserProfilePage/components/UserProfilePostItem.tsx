eca5d8899f62ed7f98712f2a8e0a3260e517dce6
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-17 17:42:51 +0300

    fix warning

--- src/components/pages/UserProfilePage/components/UserProfilePostItem.tsx
+++ src/components/pages/UserProfilePage/components/UserProfilePostItem.tsx
@@ -3,18 +3,18 @@ import PostItemCard from "../../../post/list/components/PostItemCard";
 import type { IPost } from "../../../post/list/types";
 
 type UserProfilePostItemProps = {
-	post: IPost;
-	setPosts: React.Dispatch<React.SetStateAction<IPost[]>>;
+   post: IPost;
+   setPosts: React.Dispatch<React.SetStateAction<IPost[]>>;
 };
 
 const UserProfilePostItem = ({ post, setPosts }: UserProfilePostItemProps) => {
-	return (
-		<Col key={post.id} xs={24} sm={24} md={24} lg={8} xl={8}>
-			<div style={{ width: "100%", height: "auto" }}>
-				<PostItemCard key={post.id} post={post} setPosts={setPosts} />
-			</div>
-		</Col>
-	);
+   return (
+      <Col key={post.id} xs={24} sm={24} md={24} lg={8} xl={8}>
+         <div style={{ width: "100%", height: "auto" }}>
+            <PostItemCard key={post.id} post={post} setPosts={setPosts} />
+         </div>
+      </Col>
+   );
 };
 
 export default UserProfilePostItem;
--- src/components/pages/UserProfilePage/components/UserProfilePostList.tsx
+++ src/components/pages/UserProfilePage/components/UserProfilePostList.tsx
@@ -1,25 +1,27 @@
 import { Row } from "antd";
 import type { IPost } from "../../../post/list/types";
 import UserProfilePostItem from "./UserProfilePostItem";
+import React from "react";
 
 type UserProfilePostListProps = {
-	posts: IPost[];
+   posts: IPost[];
+   setPosts: React.Dispatch<React.SetStateAction<IPost[]>>;
 };
 
-const UserProfilePostList = ({ posts }: UserProfilePostListProps) => {
-	if (posts.length <= 0) {
-		return null;
-	}
+const UserProfilePostList = ({ posts, setPosts }: UserProfilePostListProps) => {
+   if (posts.length <= 0) {
+      return null;
+   }
 
-	return (
-		<>
-			<Row gutter={[16, 16]}>
-				{posts.map((post) => (
-					<UserProfilePostItem key={post.id} post={post} />
-				))}
-			</Row>
-		</>
-	);
+   return (
+      <>
+         <Row gutter={[16, 16]}>
+            {posts.map((post) => (
+               <UserProfilePostItem key={post.id} post={post} setPosts={setPosts} />
+            ))}
+         </Row>
+      </>
+   );
 };
 
 export default UserProfilePostList;
