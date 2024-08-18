236676ac74c7ed183c0caab44c4a8526f866c1f3
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-18 13:17:05 +0300

    remove number from button in tag input

--- src/components/post/create/components/Tags/TagInput.tsx
+++ src/components/post/create/components/Tags/TagInput.tsx
@@ -2,30 +2,30 @@ import { Button, Flex, Input } from "antd";
 import { useState } from "react";
 
 type TagInputProps = {
-	tags: string[];
-	setTags: React.Dispatch<React.SetStateAction<string[]>>;
+   tags: string[];
+   setTags: React.Dispatch<React.SetStateAction<string[]>>;
 };
 
 const TagInput = ({ tags, setTags }: TagInputProps) => {
-	const [newTag, setNewTag] = useState<string>("");
+   const [newTag, setNewTag] = useState<string>("");
 
-	const addTag = () => {
-		if (newTag && !tags.includes(newTag)) {
-			setTags([...tags, newTag]);
-			setNewTag("");
-		}
-	};
+   const addTag = () => {
+      if (newTag && !tags.includes(newTag)) {
+         setTags([...tags, newTag]);
+         setNewTag("");
+      }
+   };
 
-	return (
-		<Flex gap="small">
-			<Input
-				value={newTag}
-				onChange={(e) => setNewTag(e.target.value)}
-				placeholder="Enter tag"
-			/>
-			<Button onClick={addTag}>Add 2</Button>
-		</Flex>
-	);
+   return (
+      <Flex gap="small">
+         <Input
+            value={newTag}
+            onChange={(e) => setNewTag(e.target.value)}
+            placeholder="Enter tag"
+         />
+         <Button onClick={addTag}>Add</Button>
+      </Flex>
+   );
 };
 
 export default TagInput;
