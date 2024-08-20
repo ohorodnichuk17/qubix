92bb6b551a553e4187975d64d770d87245242239
kutukov2000 <kutukov16.2005@gmail.com>
2024-08-20 10:14:59 +0300

    refactor: simplify sendFriendRequest function 📦

--- src/components/featured/SendFriendRequestButton/SendFriendRequestButton.tsx
+++ src/components/featured/SendFriendRequestButton/SendFriendRequestButton.tsx
@@ -1,8 +1,6 @@
 import { Button, type ButtonProps, message } from "antd";
 import { useState } from "react";
 import { apiClient } from "../../../utils/api/apiClient";
-import { useAppSelector } from "../../../hooks/redux";
-import type { ISendFriendRequest } from "./types";
 
 type SendFriendRequestButtonProps = ButtonProps & {
 	friendId: string;
@@ -14,27 +12,16 @@ const SendFriendRequestButton: React.FC<SendFriendRequestButtonProps> = ({
 	afterSendRequestFn,
 	...buttonProps
 }) => {
-	const { user } = useAppSelector((state) => state.account);
 	const [loading, setLoading] = useState<boolean>(false);
 
 	const sendFriendRequest = () => {
-		if (user?.id === undefined) {
-			message.error("Send friend request error");
-			return;
-		}
-
 		if (friendId === null) {
 			return;
 		}
 
-		const sendFriendRequestBody: ISendFriendRequest = {
-			friendId,
-			userId: user?.id,
-		};
-
 		setLoading(true);
 		apiClient
-			.post("/api/friends/send-friend-request", sendFriendRequestBody)
+			.post("/api/friends/send-request", { friendId })
 			.then(() => {
 				message.success("Request successfully sended!");
 				afterSendRequestFn();
