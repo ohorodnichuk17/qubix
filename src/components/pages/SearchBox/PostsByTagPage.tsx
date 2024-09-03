4b4ca5848cc013b5d093a966397830bfd20225c4
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-03 17:36:48 +0300

    fix: add usage of totalCount state to resolve warnings

--- src/components/pages/SearchBox/PostsByTagPage.tsx
+++ src/components/pages/SearchBox/PostsByTagPage.tsx
@@ -3,10 +3,11 @@ import { useLocation } from "react-router-dom";
 import { apiClient } from "../../../utils/api/apiClient";
 import type { IPost } from "../../post/list/types";
 import PostItemCard from "../../post/list/components/PostItemCard";
-import {message} from "antd";
+import { message } from "antd";
 
 const PostsByTagPage = () => {
    const [posts, setPosts] = useState<IPost[]>([]);
+   const [totalCount, setTotalCount] = useState<number>(0); // Add state for totalCount
    const location = useLocation();
 
    useEffect(() => {
@@ -16,26 +17,33 @@ const PostsByTagPage = () => {
       if (tag) {
          console.log(`Searching for posts with tag: ${tag}`);
          apiClient
-            .post(`/api/post/search-posts-by-tags?tag=${tag}`)
-            .then((res) => {
-               setPosts(res.data);
-            })
-            .catch(() => {
-               message.error("Error fetching posts by tag:");
-            });
+             .post(`/api/post/search-posts-by-tags?tag=${tag}`)
+             .then((res) => {
+                setPosts(res.data);
+                setTotalCount(res.data.length);
+             })
+             .catch(() => {
+                message.error("Error fetching posts by tag:");
+             });
       }
    }, [location.search]);
 
    return (
-      <div>
-         {posts.length > 0 ? (
-            posts.map((post) => (
-               <PostItemCard key={post.id} post={post} setPosts={setPosts}/>
-            ))
-         ) : (
-            <p>No posts found for this tag.</p>
-         )}
-      </div>
+       <div>
+          <p>Total Posts: {totalCount}</p>
+          {posts.length > 0 ? (
+              posts.map((post) => (
+                  <PostItemCard
+                      key={post.id}
+                      post={post}
+                      setPosts={setPosts}
+                      setTotalCount={setTotalCount}
+                  />
+              ))
+          ) : (
+              <p>No posts found for this tag.</p>
+          )}
+       </div>
    );
 };
 
