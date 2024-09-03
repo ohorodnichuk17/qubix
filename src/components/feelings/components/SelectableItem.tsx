2ae27ccd0407e83e4151e07667884d552b0eb95d
kutukov2000 <kutukov16.2005@gmail.com>
2024-09-03 14:17:51 +0300

    refactor: use React.memo to prevent unnecessary re-renders of the SelectableItem component 📦

--- src/components/feelings/components/SelectableItem.tsx
+++ src/components/feelings/components/SelectableItem.tsx
@@ -1,34 +1,36 @@
 import { Flex } from "antd";
+import React from "react";
 
 type SelectableItemProps = {
-	item: { name: string; emoji: string };
-	isSelected: boolean;
-	onClick: () => void;
+    item: { name: string; emoji: string };
+    isSelected: boolean;
+    onClick: () => void;
 };
 
-const SelectableItem = ({ item, isSelected, onClick }: SelectableItemProps) => {
-	return (
-		<Flex
-			gap="small"
-			align="center"
-			style={{
-				width: "45%",
-				padding: "5px",
-				borderRadius: "8px",
-				background: isSelected ? "gray" : "none",
-				color: isSelected ? "white" : "black",
-				transition: ".7s",
-			}}
-			onClick={onClick}
-		>
-			<img
-				src={item.emoji}
-				alt="Feeling/Action icon (emoji)"
-				className="h-50px"
-			/>
-			<span>{item.name}</span>
-		</Flex>
-	);
-};
+const SelectableItem = React.memo(
+    ({ item, isSelected, onClick }: SelectableItemProps) => (
+        <Flex
+            gap="small"
+            align="center"
+            style={{
+                width: "45%",
+                padding: "5px",
+                borderRadius: "8px",
+                background: isSelected ? "gray" : "none",
+                color: isSelected ? "white" : "black",
+                transition: ".7s",
+            }}
+            onClick={onClick}
+        >
+            <img
+                src={item.emoji}
+                alt="Feeling/Action icon (emoji)"
+                className="h-50px"
+            />
+            <span>{item.name}</span>
+        </Flex>
+    ),
+    (prevProps, nextProps) => prevProps.isSelected === nextProps.isSelected,
+);
 
 export default SelectableItem;
