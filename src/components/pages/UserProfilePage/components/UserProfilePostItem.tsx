93671179c3af925eed326e91d76585570f5df214
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-03 17:40:01 +0300

    fix: add setTotalCount prop to UserProfilePostItem and PostItemCard components

--- src/components/pages/UserProfilePage/components/UserProfilePostItem.tsx
+++ src/components/pages/UserProfilePage/components/UserProfilePostItem.tsx
@@ -3,18 +3,19 @@ import PostItemCard from "../../../post/list/components/PostItemCard";
 import type { IPost } from "../../../post/list/types";
 
 type UserProfilePostItemProps = {
-   post: IPost;
-   setPosts: React.Dispatch<React.SetStateAction<IPost[]>>;
+    post: IPost;
+    setPosts: React.Dispatch<React.SetStateAction<IPost[]>>;
+    setTotalCount: React.Dispatch<React.SetStateAction<number>>;
 };
 
-const UserProfilePostItem = ({ post, setPosts }: UserProfilePostItemProps) => {
-   return (
-      <Col key={post.id} xs={24} sm={24} md={24} lg={8} xl={8}>
-         <div style={{ width: "100%", height: "auto" }}>
-            <PostItemCard key={post.id} post={post} setPosts={setPosts} />
-         </div>
-      </Col>
-   );
+const UserProfilePostItem = ({ post, setPosts, setTotalCount }: UserProfilePostItemProps) => {
+    return (
+        <Col key={post.id} xs={24} sm={24} md={24} lg={8} xl={8}>
+            <div style={{ width: "100%", height: "auto" }}>
+                <PostItemCard key={post.id} post={post} setPosts={setPosts} setTotalCount={setTotalCount} />
+            </div>
+        </Col>
+    );
 };
 
 export default UserProfilePostItem;
