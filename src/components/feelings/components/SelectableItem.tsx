192a416a96f5836075b37f1ec0db4f4763db2259
kutukov2000 <kutukov16.2005@gmail.com>
2024-09-03 14:25:17 +0300

    style: hover background effect for SelectableItem 💎

--- src/components/feelings/components/SelectableItem.tsx
+++ src/components/feelings/components/SelectableItem.tsx
@@ -2,35 +2,28 @@ import { Flex } from "antd";
 import React from "react";
 
 type SelectableItemProps = {
-    item: { name: string; emoji: string };
-    isSelected: boolean;
-    onClick: () => void;
+	item: { name: string; emoji: string };
+	isSelected: boolean;
+	onClick: () => void;
 };
 
 const SelectableItem = React.memo(
-    ({ item, isSelected, onClick }: SelectableItemProps) => (
-        <Flex
-            gap="small"
-            align="center"
-            style={{
-                width: "45%",
-                padding: "5px",
-                borderRadius: "8px",
-                background: isSelected ? "gray" : "none",
-                color: isSelected ? "white" : "black",
-                transition: ".7s",
-            }}
-            onClick={onClick}
-        >
-            <img
-                src={item.emoji}
-                alt="Feeling/Action icon (emoji)"
-                className="h-50px"
-            />
-            <span>{item.name}</span>
-        </Flex>
-    ),
-    (prevProps, nextProps) => prevProps.isSelected === nextProps.isSelected,
+	({ item, isSelected, onClick }: SelectableItemProps) => (
+		<Flex
+			className="selectable-item-flex"
+			gap="small"
+			align="center"
+			style={{
+				background: isSelected ? "gray" : "none",
+				color: isSelected ? "white" : "black",
+			}}
+			onClick={onClick}
+		>
+			<img src={item.emoji} alt="Feeling/Action icon (emoji)" />
+			<span>{item.name}</span>
+		</Flex>
+	),
+	(prevProps, nextProps) => prevProps.isSelected === nextProps.isSelected,
 );
 
 export default SelectableItem;
