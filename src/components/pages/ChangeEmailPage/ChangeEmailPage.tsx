021f3c351f37a2caeef734be50e8de40ce8c8732
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-09-03 17:13:42 +0300

    feat: add responsive design to ChangeEmailPage component and hide image on mobile devices

--- src/components/pages/ChangeEmailPage/ChangeEmailPage.tsx
+++ src/components/pages/ChangeEmailPage/ChangeEmailPage.tsx
@@ -1,22 +1,22 @@
+import React from "react";
 import { MailOutlined } from "@ant-design/icons";
-import { Button, Card, Flex, Form, Input, message } from "antd";
-import type React from "react";
+import {Button, Card, Col, Flex, Form, Input, message, Row} from "antd";
 import { Link, useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../../hooks/redux";
-import type { IChangeEmail } from "../../../interfaces/account";
 import { apiClient } from "../../../utils/api/apiClient";
 import { changeEmail } from "../../../utils/images";
+import {IChangeEmail} from "../../../interfaces/account";
+import './styles.css'
 
 const ChangeEmailPage: React.FC = () => {
    const account = useAppSelector((state) => state.account);
-
    const navigate = useNavigate();
 
    const onFinish = (values: IChangeEmail) => {
       if (
-         account?.token === null ||
-         account?.user?.id === null ||
-         account?.user?.id === undefined
+          account?.token === null ||
+          account?.user?.id === null ||
+          account?.user?.id === undefined
       ) {
          message.error("Change email error. Try later");
          return;
@@ -26,58 +26,60 @@ const ChangeEmailPage: React.FC = () => {
       values.userId = account?.user?.id;
 
       apiClient
-         .post("/api/authentication/change-email", values)
-         .then((res) => {
-            console.log(res);
-            if (res.status === 200) {
-               message.success("Success").then(() => navigate("/"));
-            }
-         })
-         .catch(() => {
-            message.error("error");
-         });
+          .post("/api/authentication/change-email", values)
+          .then((res) => {
+             console.log(res);
+             if (res.status === 200) {
+                message.success("Success").then(() => navigate("/"));
+             }
+          })
+          .catch(() => {
+             message.error("error");
+          });
    };
 
    return (
-      <Flex
-         justify="center"
-         align="center"
-         wrap="wrap"
-         className="auth-pages-flex"
-      >
-         <div>
-            <img
-               src={changeEmail}
-               alt="forgotPasswordImage"
-               className="auth-pages-img"
-            />
-         </div>
-         <Card style={{ width: "400px" }}>
-            <h3 style={{ fontSize: 32 }}>Email change</h3>
-            <p style={{ fontSize: 18 }}>
-               Change the email address for your Qubix account
-            </p>
-            <Form onFinish={onFinish} layout="vertical" requiredMark={false}>
-               <Form.Item label="Email" name="email">
-                  <Input prefix={<MailOutlined />} placeholder="Email address" />
-               </Form.Item>
-
-               <Form.Item>
-                  <Button htmlType="submit" style={{ color: "white", width: "100%" }}>
-                     Change my email address
-                  </Button>
-               </Form.Item>
-
-               <Form.Item>
-                  <Flex justify="center">
-                     <Link to="/" style={{ color: "#FF6347" }}>
-                        Back to Main page
-                     </Link>
-                  </Flex>
-               </Form.Item>
-            </Form>
-         </Card>
-      </Flex>
+       <Flex
+           justify="center"
+           align="center"
+           wrap="wrap"
+           className="auth-pages-flex"
+       >
+          <div>
+             <img
+                 src={changeEmail}
+                 alt="forgotPasswordImage"
+                 className="auth-pages-img"
+             />
+          </div>
+          <Card style={{ width: "100%", maxWidth: "400px" }}>
+             <h3 style={{ fontSize: "2rem", textAlign: "center" }}>Email Change</h3>
+             <p style={{ fontSize: "1rem", textAlign: "center" }}>
+                Change the email address for your Qubix account
+             </p>
+             <Form onFinish={onFinish} layout="vertical" requiredMark={false}>
+                <Row gutter={16}>
+                   <Col span={24}>
+                      <Form.Item label="Email" name="email">
+                         <Input prefix={<MailOutlined />} placeholder="Email address" />
+                      </Form.Item>
+                   </Col>
+                </Row>
+                <Form.Item>
+                   <Button htmlType="submit" style={{ color: "white", width: "100%" }}>
+                      Change my email address
+                   </Button>
+                </Form.Item>
+                <Form.Item>
+                   <Flex justify="center">
+                      <Link to="/" style={{ color: "#FF6347" }}>
+                         Back to Main page
+                      </Link>
+                   </Flex>
+                </Form.Item>
+             </Form>
+          </Card>
+       </Flex>
    );
 };
 
