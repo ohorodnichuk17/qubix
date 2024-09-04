57266376b04311e4474b56de72a43f5e69252057
kutukov2000 <kutukov16.2005@gmail.com>
2024-09-04 11:44:23 +0300

    style: online status badge & delete post button adaptive 💎

--- src/components/post/list/components/PostItemCard.tsx
+++ src/components/post/list/components/PostItemCard.tsx
@@ -1,6 +1,23 @@
 import { useEffect, useState } from "react";
-import { Card, Flex, Avatar, Divider, Carousel, Tag, Badge, message, Tooltip, Popconfirm } from "antd";
-import { CommentOutlined, DeleteTwoTone, LikeFilled, SmileTwoTone } from "@ant-design/icons";
+import {
+	Card,
+	Flex,
+	Avatar,
+	Divider,
+	Carousel,
+	Tag,
+	Badge,
+	message,
+	Tooltip,
+	Popconfirm,
+	Grid,
+} from "antd";
+import {
+	CommentOutlined,
+	DeleteTwoTone,
+	LikeFilled,
+	SmileTwoTone,
+} from "@ant-design/icons";
 import { APP_ENV } from "../../../../env";
 import { avatar, likeImg, locationImg } from "../../../../utils/images";
 import { getRandomTagColor } from "../../create/components/Tags/TagsList";
@@ -11,264 +28,308 @@ import { apiClient } from "../../../../utils/api/apiClient";
 import Arrow from "../../../featured/Arrow/Arrow";
 import { useAppSelector } from "../../../../hooks/redux";
 import EmojiPicker, { type EmojiClickData } from "emoji-picker-react";
-import { IComment, ILike, IPost } from "../types.ts";
-import { getActionImage, getFeelingImage, getPublicationDate } from "../utils.ts";
+import type { IComment, ILike, IPost } from "../types.ts";
+import {
+	getActionImage,
+	getFeelingImage,
+	getPublicationDate,
+} from "../utils.ts";
 
 type PostItemCardProps = {
-   post: IPost;
-   setPosts: React.Dispatch<React.SetStateAction<IPost[]>>;
-   setTotalCount?: React.Dispatch<React.SetStateAction<number>>;
+	post: IPost;
+	setPosts: React.Dispatch<React.SetStateAction<IPost[]>>;
+	setTotalCount?: React.Dispatch<React.SetStateAction<number>>;
 };
 
 const PostItemCard = ({ post, setPosts, setTotalCount }: PostItemCardProps) => {
-   const { user } = useAppSelector((state) => state.account);
-   const [commentsVisibility, setCommentsVisibility] = useState<boolean>(false);
-   const [comments, setComments] = useState<IComment[]>([]);
-   const [isLiked, setIsLiked] = useState<boolean>(false);
-   const [showPicker, setShowPicker] = useState<boolean>(false);
+	const { user } = useAppSelector((state) => state.account);
+	const [commentsVisibility, setCommentsVisibility] = useState<boolean>(false);
+	const [comments, setComments] = useState<IComment[]>([]);
+	const [isLiked, setIsLiked] = useState<boolean>(false);
+	const [showPicker, setShowPicker] = useState<boolean>(false);
 
-   useEffect(() => {
-      apiClient.get(`api/comment/${post.id}`).then((res) => {
-         setComments(res.data);
-      });
+	const screens = Grid.useBreakpoint();
 
-      for (const like of post.likes) {
-         if (like.userId === user?.id) {
-            setIsLiked(true);
-            return;
-         }
-      }
-   }, [post.id, post.likes, user]);
+	useEffect(() => {
+		apiClient.get(`api/comment/${post.id}`).then((res) => {
+			setComments(res.data);
+		});
 
-   if (!post || !post.user) {
-      return null;
-   }
+		for (const like of post.likes) {
+			if (like.userId === user?.id) {
+				setIsLiked(true);
+				return;
+			}
+		}
+	}, [post.id, post.likes, user]);
 
-   const likePost = () => {
-      apiClient.post<ILike>("api/like", { postId: post.id }).then((res) => {
-         setPosts((prevPosts) =>
-             prevPosts.map((prevPost) =>
-                 prevPost.id === post.id
-                     ? { ...post, likes: [...prevPost.likes, res.data] }
-                     : prevPost
-             )
-         );
-      });
-   };
+	if (!post || !post.user) {
+		return null;
+	}
 
-   const onEmojiClick = (event: EmojiClickData) => {
-      const data = {
-         postId: post.id,
-         emoji: event.emoji,
-      };
-      apiClient.post("/api/reaction", data);
-      setShowPicker(false);
-   };
+	const likePost = () => {
+		apiClient.post<ILike>("api/like", { postId: post.id }).then((res) => {
+			setPosts((prevPosts) =>
+				prevPosts.map((prevPost) =>
+					prevPost.id === post.id
+						? { ...post, likes: [...prevPost.likes, res.data] }
+						: prevPost,
+				),
+			);
+		});
+	};
 
-   const unlikePost = () => {
-      try {
-         apiClient.delete("/api/like", { data: { postId: post.id } });
-         setIsLiked(false);
+	const onEmojiClick = (event: EmojiClickData) => {
+		const data = {
+			postId: post.id,
+			emoji: event.emoji,
+		};
+		apiClient.post("/api/reaction", data);
+		setShowPicker(false);
+	};
 
-         setPosts((prevPosts) =>
-             prevPosts.map((prevPost) =>
-                 prevPost.id === post.id
-                     ? {
-                        ...prevPost,
-                        likes: prevPost.likes.filter(
-                            (like) => like.userId !== user?.id
-                        ),
-                     }
-                     : prevPost
-             )
-         );
-      } catch (error) {
-         message.error("Unlike post error!");
-      }
-   };
+	const unlikePost = () => {
+		try {
+			apiClient.delete("/api/like", { data: { postId: post.id } });
+			setIsLiked(false);
 
-   const deletePost = () => {
-      try {
-         apiClient.delete("api/post", { data: { id: post.id } });
-         setPosts((prevPosts) =>
-             prevPosts.filter((postFromList) => postFromList.id !== post.id)
-         );
-         if (setTotalCount) setTotalCount((totalCount) => totalCount - 1);
-         message.success("Post successfully deleted!");
-      } catch (error) {
-         message.error("Post deletion error!");
-      }
-   };
+			setPosts((prevPosts) =>
+				prevPosts.map((prevPost) =>
+					prevPost.id === post.id
+						? {
+								...prevPost,
+								likes: prevPost.likes.filter(
+									(like) => like.userId !== user?.id,
+								),
+							}
+						: prevPost,
+				),
+			);
+		} catch (error) {
+			message.error("Unlike post error!");
+		}
+	};
 
-   return (
-       <Card
-           key={post.id}
-           className="card-container"
-           style={{ maxWidth: "600px", width: "100%", margin: "auto", position: "relative" }}
-       >
-          {/* Top icons container */}
-          <div className="top-icons">
-             {post.user.isOnline && (
-                 <Badge color="green" count={"online"} className="online-badge" />
-             )}
-             {post.userId === user?.id && (
-                 <Popconfirm
-                     title="Delete post ?"
-                     onConfirm={deletePost}
-                     okText="Yes"
-                     cancelText="No"
-                     className="trash-icon"
-                 >
-                    <DeleteTwoTone />
-                 </Popconfirm>
-             )}
-          </div>
+	const deletePost = () => {
+		try {
+			apiClient.delete("api/post", { data: { id: post.id } });
+			setPosts((prevPosts) =>
+				prevPosts.filter((postFromList) => postFromList.id !== post.id),
+			);
+			if (setTotalCount) setTotalCount((totalCount) => totalCount - 1);
+			message.success("Post successfully deleted!");
+		} catch (error) {
+			message.error("Post deletion error!");
+		}
+	};
 
-          <Flex vertical gap="small">
-             <Flex justify="space-between" align="center" gap="small">
-                <Flex align="center" gap="small">
-                   <NavLink to={`/profile?userId=${post.user.id}`}>
-                      <Avatar
-                          size={60}
-                          src={
-                             post.user.avatar === null
-                                 ? avatar
-                                 : `${APP_ENV.BASE_URL}/images/avatars/${post.user.avatar}`
-                          }
-                      />
-                   </NavLink>
-                   <Flex vertical>
-                      <Flex align="center" gap="small">
-                         <NavLink to={`/profile?userId=${post.user.id}`} style={{ color: "black" }}>
-                  <span style={{ fontWeight: 600, fontSize: 20 }}>
-                    {`${post.user.firstName} ${post.user.lastName}`}
-                  </span>
-                         </NavLink>
-                      </Flex>
-                      <div className="date-container">
-                         <span>{getPublicationDate(post.createdAt)}</span>
-                      </div>
-                   </Flex>
-                </Flex>
-             </Flex>
+	const isScreenSmallerThatMd =
+		(screens.xs || screens.sm) &&
+		!screens.md &&
+		!screens.lg &&
+		!screens.xl &&
+		!screens.xxl;
 
-             <Divider style={{ margin: 0 }} />
+	return (
+		<Card
+			key={post.id}
+			className="card-container"
+			style={{
+				maxWidth: "600px",
+				width: "100%",
+				margin: "auto",
+				position: "relative",
+			}}
+		>
+			{isScreenSmallerThatMd && (
+				<Flex
+					justify="space-between"
+					style={{ width: "100%", marginBottom: 5 }}
+				>
+					{post.user.isOnline && <Badge color="green" count={"online"} />}
+					{post.userId === user?.id && (
+						<Popconfirm
+							title="Delete post ?"
+							onConfirm={deletePost}
+							okText="Yes"
+							cancelText="No"
+						>
+							<DeleteTwoTone />
+						</Popconfirm>
+					)}
+				</Flex>
+			)}
 
-             {post.action && (
-                 <Flex align="center" gap="small">
-                    <img
-                        src={getActionImage(post.action, post.subAction)}
-                        alt="Action icon"
-                        style={{ height: 35, width: 35 }}
-                    />
-                    <span>{post.action.name}</span>
+			<Flex vertical gap="small">
+				<Flex justify="space-between">
+					<Flex align="center" gap="small">
+						<NavLink to={`/profile?userId=${post.user.id}`}>
+							<Avatar
+								size={60}
+								src={
+									post.user.avatar === null
+										? avatar
+										: `${APP_ENV.BASE_URL}/images/avatars/${post.user.avatar}`
+								}
+							/>
+						</NavLink>
+						<Flex vertical>
+							<Flex align="center" gap="small">
+								<NavLink
+									to={`/profile?userId=${post.user.id}`}
+									style={{ color: "black" }}
+								>
+									<span style={{ fontWeight: 600, fontSize: 20 }}>
+										{`${post.user.firstName} ${post.user.lastName}`}
+									</span>
+								</NavLink>
+							</Flex>
+							<span>{getPublicationDate(post.createdAt)}</span>
+						</Flex>
+						{!isScreenSmallerThatMd && (
+							<>
+								{post.user.isOnline ? (
+									<Badge color="green" count={"online"} />
+								) : (
+									<Badge color="gray" count={"offline"} />
+								)}
+							</>
+						)}
+					</Flex>
+					{!isScreenSmallerThatMd && (
+						<Flex justify="end">
+							{post.userId === user?.id && (
+								<Popconfirm
+									title="Delete post ?"
+									onConfirm={deletePost}
+									okText="Yes"
+									cancelText="No"
+								>
+									<DeleteTwoTone style={{ fontSize: 18 }} />
+								</Popconfirm>
+							)}
+						</Flex>
+					)}
+				</Flex>
 
-                    {post.subAction && <span>{post.subAction.name}</span>}
-                 </Flex>
-             )}
+				<Divider style={{ margin: 0 }} />
 
-             {post.feeling && (
-                 <Flex align="center" gap="small">
-                    <img
-                        src={getFeelingImage(post.feeling)}
-                        alt="Feeling icon"
-                        style={{ height: 35, width: 35 }}
-                    />
-                    <span>{post.feeling.name}</span>
-                 </Flex>
-             )}
+				{post.action && (
+					<Flex align="center" gap="small">
+						<img
+							src={getActionImage(post.action, post.subAction)}
+							alt="Action icon"
+							style={{ height: 35, width: 35 }}
+						/>
+						<span>{post.action.name}</span>
 
-             <Carousel
-                 arrows
-                 draggable
-                 infinite
-                 nextArrow={<Arrow direction="right" />}
-                 prevArrow={<Arrow direction="left" />}
-             >
-                {post.images.map((image) => (
-                    <img
-                        key={image.id}
-                        src={`${APP_ENV.BASE_URL}/images/posts/${image.imagePath}`}
-                        className="post-preview-img"
-                        alt="Post images"
-                        style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                ))}
-             </Carousel>
+						{post.subAction && <span>{post.subAction.name}</span>}
+					</Flex>
+				)}
 
-             {post.location && (
-                 <Flex align="center" gap="small">
-                    <img src={locationImg} height={35} alt="Location icon" />
-                    <span>{post.location}</span>
-                 </Flex>
-             )}
+				{post.feeling && (
+					<Flex align="center" gap="small">
+						<img
+							src={getFeelingImage(post.feeling)}
+							alt="Feeling icon"
+							style={{ height: 35, width: 35 }}
+						/>
+						<span>{post.feeling.name}</span>
+					</Flex>
+				)}
 
-             {post.content !== "undefined" && (
-                 <p style={{ margin: 0 }}>{post.content}</p>
-             )}
+				<Carousel
+					arrows
+					draggable
+					infinite
+					nextArrow={<Arrow direction="right" />}
+					prevArrow={<Arrow direction="left" />}
+				>
+					{post.images.map((image) => (
+						<img
+							key={image.id}
+							src={`${APP_ENV.BASE_URL}/images/posts/${image.imagePath}`}
+							className="post-preview-img"
+							alt="Post images"
+							style={{ maxWidth: "100%", height: "auto" }}
+						/>
+					))}
+				</Carousel>
 
-             {post.tags && (
-                 <Flex wrap="wrap" gap="small">
-                    {post.tags.map((tag) => (
-                        <Tag
-                            key={tag}
-                            color={getRandomTagColor()}
-                            style={{
-                               margin: 0,
-                               width: "fit-content",
-                               fontSize: "14px",
-                            }}
-                        >
-                           {`#${tag}`}
-                        </Tag>
-                    ))}
-                 </Flex>
-             )}
+				{post.location && (
+					<Flex align="center" gap="small">
+						<img src={locationImg} height={35} alt="Location icon" />
+						<span>{post.location}</span>
+					</Flex>
+				)}
 
-             <Flex gap="small">
-                {isLiked ? (
-                    <Tooltip title="Unlike post">
-                       <Flex className="post-actions-flex" onClick={unlikePost}>
-                          <LikeFilled style={{ color: "red" }} />
-                          <span>{post.likes.length}</span>
-                       </Flex>
-                    </Tooltip>
-                ) : (
-                    <Flex className="post-actions-flex" onClick={likePost}>
-                       <img src={likeImg} alt="Like" width={30} />
-                       <span>{post.likes.length}</span>
-                    </Flex>
-                )}
-                <Flex
-                    className="post-actions-flex"
-                    onClick={() => setShowPicker(!showPicker)}
-                >
-                   <SmileTwoTone style={{ fontSize: 20 }} />
-                   <span>Reactions</span>
-                </Flex>
+				{post.content !== "undefined" && (
+					<p style={{ margin: 0 }}>{post.content}</p>
+				)}
 
-                <Tooltip title={commentsVisibility ? "Hide comments" : "Show comments"}>
-                   <Flex
-                       gap="small"
-                       className="post-actions-flex"
-                       onClick={() => setCommentsVisibility(!commentsVisibility)}
-                   >
-                      <CommentOutlined />
-                      <span>{comments.length > 0 ? comments.length : ""}</span>
-                   </Flex>
-                </Tooltip>
-             </Flex>
-             <EmojiPicker open={showPicker} onEmojiClick={onEmojiClick} />
+				{post.tags && (
+					<Flex wrap="wrap" gap="small">
+						{post.tags.map((tag) => (
+							<Tag
+								key={tag}
+								color={getRandomTagColor()}
+								style={{
+									margin: 0,
+									width: "fit-content",
+									fontSize: "14px",
+								}}
+							>
+								{`#${tag}`}
+							</Tag>
+						))}
+					</Flex>
+				)}
 
-             <AddCommentForm post={post} setComments={setComments} />
+				<Flex gap="small">
+					{isLiked ? (
+						<Tooltip title="Unlike post">
+							<Flex className="post-actions-flex" onClick={unlikePost}>
+								<LikeFilled style={{ color: "red" }} />
+								<span>{post.likes.length}</span>
+							</Flex>
+						</Tooltip>
+					) : (
+						<Flex className="post-actions-flex" onClick={likePost}>
+							<img src={likeImg} alt="Like" width={30} />
+							<span>{post.likes.length}</span>
+						</Flex>
+					)}
+					<Flex
+						className="post-actions-flex"
+						onClick={() => setShowPicker(!showPicker)}
+					>
+						<SmileTwoTone style={{ fontSize: 20 }} />
+						<span>Reactions</span>
+					</Flex>
 
-             {commentsVisibility && (
-                 <CommentsList comments={comments} setComments={setComments} />
-             )}
-          </Flex>
-       </Card>
-   );
+					<Tooltip
+						title={commentsVisibility ? "Hide comments" : "Show comments"}
+					>
+						<Flex
+							gap="small"
+							className="post-actions-flex"
+							onClick={() => setCommentsVisibility(!commentsVisibility)}
+						>
+							<CommentOutlined />
+							<span>{comments.length > 0 ? comments.length : ""}</span>
+						</Flex>
+					</Tooltip>
+				</Flex>
+				<EmojiPicker open={showPicker} onEmojiClick={onEmojiClick} />
+
+				<AddCommentForm post={post} setComments={setComments} />
+
+				{commentsVisibility && (
+					<CommentsList comments={comments} setComments={setComments} />
+				)}
+			</Flex>
+		</Card>
+	);
 };
 
 export default PostItemCard;
-
