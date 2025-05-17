46e0eb39493ba8d0351c965b0ad8e4258c17630a
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2025-05-17 11:56:51 +0300

    move facebook-server files into qubix-server directory

--- Facebook_Contracts/Friends/FriendRequest.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace Facebook.Contracts.Friends;
-
-public record FriendRequest
-{
-    [Required(ErrorMessage = "{PropertyName} must not be empty")]
-    public Guid FriendId { get; set; }
-}
